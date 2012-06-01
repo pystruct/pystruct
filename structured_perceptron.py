09d433481345f6e0e9854fa6acea530cfeaee592
Andy <amueller@ais.uni-bonn.de>
2012-06-01 11:48:57 +0200

    slight refactoring, latent variable svm

--- structured_perceptron.py
+++ structured_perceptron.py
@@ -36,19 +36,11 @@ class StructuredPerceptron(object):
         return prediction
 
 
-class LatentStructuredPerceptron(object):
-    def __init__(self, problem, max_iter=100):
-        self.max_iter = max_iter
-        self.problem = problem
-
+class LatentStructuredPerceptron(StructuredPerceptron):
     def fit(self, X, Y):
         n_samples = len(X)
         size_psi = self.problem.size_psi
         w = np.zeros(size_psi)
-        #w = np.array([-4736.79779604, -1238.3792176,  -4911.2104873 , -1217.90553114,   625.07777778,
-                  #1845.68214286, -2484.36666667,  1311.6468254,  -1567.50555556 ,  690.67460317,
-                   #-1314.63174603,  -193.  ,        2118.7702381,  -3233.30952381]) / 100.
-
         try:
             for iteration in xrange(self.max_iter):
                 alpha = 0.01 / (1 + iteration)
--- structured_svm.py
+++ structured_svm.py
@@ -11,6 +11,7 @@ import cvxopt
 import cvxopt.solvers
 from IPython.core.debugger import Tracer
 
+#cvxopt.solvers.options['show_progress'] = False
 tracer = Tracer()
 
 
@@ -60,20 +61,88 @@ class StructuredSVM(object):
             new_constraints = 0
             current_loss = 0.
             for x, y in zip(X, Y):
-                y_hat = self.problem.inference(x, w)
-                if (y_hat != y).any():
+                y_hat = self.problem.loss_augmented_inference(x, y, w)
+                loss = self.problem.loss(y, y_hat)
+                if loss:
                     constraints.append((x, y, y_hat))
                     delta_psi = psi(x, y) - psi(x, y_hat)
                     psis.append(delta_psi / 1000.)
-                    losses.append(self.problem.loss(y, y_hat))
-                    current_loss += losses[-1]
+                    losses.append(loss)
+                    current_loss += loss
                     new_constraints += 1
-                else:
-                    print("found gt!")
             if new_constraints == 0:
                 print("no loss on training set")
                 break
-            print("current loss: %f" % (current_loss / len(X)))
+            print("current loss: %f  new constraints: %d" %
+                    (current_loss / len(X), new_constraints))
+            w = self._solve_qp(constraints, psis, losses)
+
+            print(w)
+        self.w = w
+
+    def predict(self, X):
+        prediction = []
+        for x in X:
+            prediction.append(self.problem.inference(x, self.w))
+        return prediction
+
+
+class LatentStructuredSVM(StructuredSVM):
+    """Margin rescaled with l1 slack penalty."""
+    def _solve_qp(self, constraints, psis, losses):
+        psi_matrix = np.vstack(psis)
+        n_constraints = len(constraints)
+        P = cvxopt.matrix(np.dot(psi_matrix, psi_matrix.T))
+        q = cvxopt.matrix(-np.array(losses, dtype=np.float))
+        idy = np.identity(n_constraints)
+        G = cvxopt.matrix(np.vstack((-idy, idy)))
+        tmp1 = np.zeros(n_constraints)
+        tmp2 = np.ones(n_constraints) * self.C
+        h = cvxopt.matrix(np.hstack((tmp1, tmp2)))
+        # solve QP problem
+        solution = cvxopt.solvers.qp(P, q, G, h)
+
+        # Lagrange multipliers
+        a = np.ravel(solution['x'])
+
+        # Support vectors have non zero lagrange multipliers
+        sv = a > 1e-5
+        print("%d support vectors out of %d points" % (np.sum(sv),
+            n_constraints))
+        print("Coefficients at C: %d" % np.sum(self.C - a < 1e-3))
+        w = np.zeros(self.problem.size_psi)
+        for issv, dpsi, alpha in zip(sv, psis, a):
+            if not issv:
+                continue
+            w += alpha * dpsi
+        return w
+
+    def fit(self, X, Y):
+        psi = self.problem.psi
+        w = np.zeros(self.problem.size_psi)
+        constraints = []
+        psis = []
+        losses = []
+        for iteration in xrange(self.max_iter):
+            print("iteration %d" % iteration)
+            new_constraints = 0
+            current_loss = 0.
+            for x, y in zip(X, Y):
+                h = self.problem.latent(x, y, w)
+                h_hat, y_hat = self.problem.loss_augmented_inference(x, y, w)
+                loss = self.problem.loss(y, y_hat)
+                if loss:
+                    constraints.append((x, h, y, h_hat, y_hat))
+                    delta_psi = psi(x, h, y) - psi(x, h_hat, y_hat)
+                    psis.append(delta_psi / 1000.)
+                    losses.append(loss)
+                    current_loss += loss[-1]
+                    new_constraints += 1
+            if new_constraints == 0:
+                print("no loss on training set")
+                break
+            print("current loss: %f  new constraints: %d" %
+                    (current_loss / len(X), new_constraints))
             w = self._solve_qp(constraints, psis, losses)
 
             print(w)
