35d4fa60328172d17df08f100b88d9f8812e5d99
Andy <amueller@ais.uni-bonn.de>
2012-06-11 16:34:22 +0200

    fix seed

--- examples_binary_grid_crf.py
+++ examples_binary_grid_crf.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from crf import BinaryGridCRF
 #from structured_perceptron import StructuredPerceptron
 from structured_svm import StructuredSVM
+from structured_svm import SubgradientStructuredSVM
 from examples_latent_crf import make_dataset_easy_latent
 
 from IPython.core.debugger import Tracer
@@ -20,6 +21,7 @@ def make_dataset_blocks():
 
 
 def make_dataset_checker(n_samples=20):
+    np.random.seed(0)
     Y = np.ones((n_samples, 11, 13))
     Y[:, ::2, ::2] = -1
     Y[:, 1::2, 1::2] = -1
@@ -43,11 +45,12 @@ def make_dataset_big_checker(n_samples=20):
 def main():
     #X, Y = make_dataset_blocks()
     #X, Y = make_dataset_checker()
-    X, Y = make_dataset_easy_latent(n_samples=10)
-    #X, Y = make_dataset_big_checker()
+    #X, Y = make_dataset_easy_latent(n_samples=10)
+    X, Y = make_dataset_big_checker()
     crf = BinaryGridCRF()
     #clf = StructuredPerceptron(problem=crf, max_iter=100)
-    clf = StructuredSVM(problem=crf, max_iter=200, C=1000)
+    clf = StructuredSVM(problem=crf, max_iter=200, C=100)
+    #clf = SubgradientStructuredSVM(problem=crf, max_iter=2000, C=100)
     clf.fit(X, Y)
     Y_pred = clf.predict(X)
     i = 0
