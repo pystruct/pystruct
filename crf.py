fixed stupid normalization bug, more verbosity

crf.py
@@ -140,6 +140,7 @@ class MultinomialFixedGraphCRF(StructuredProblem):
     graph is given by scipy sparse adjacency matrix.
     """
     def __init__(self, n_states, graph):
+        self.inference_calls = 0
         self.n_states = n_states
         # n_states unary parameters, upper triangular for pairwise
         self.size_psi = n_states + n_states * (n_states + 1) / 2
@@ -165,15 +166,13 @@ class MultinomialFixedGraphCRF(StructuredProblem):
 
         neighbors = self.graph * labels
         pw = np.dot(neighbors.T, labels)
-        # normalize potentials
-        pw /= n_nodes
-        unaries_acc /= n_nodes
 
         feature = np.hstack([unaries_acc, pw[np.tri(self.n_states,
             dtype=np.bool)]])
         return feature
 
     def inference(self, x, w):
+        self.inference_calls += 1
         unary_params = w[:self.n_states]
         pairwise_flat = np.asarray(w[self.n_states:])
         pairwise_params = np.zeros((self.n_states, self.n_states))
@@ -182,7 +181,7 @@ class MultinomialFixedGraphCRF(StructuredProblem):
                 - np.diag(np.diag(pairwise_params))
         unaries = (-1000 * unary_params * x).astype(np.int32)
         pairwise = (-1000 * pairwise_params).astype(np.int32)
-        y = alpha_expansion_graph(self.edges, unaries, pairwise)
+        y = alpha_expansion_graph(self.edges, unaries, pairwise, random_seed=10)
         return y
 
     def loss_augmented_inference(self, x, y, w):

structured_svm.py
@@ -87,15 +87,17 @@ class StructuredSVM(object):
             for i, x, y in zip(np.arange(len(X)), X, Y):
                 y_hat = self.problem.loss_augmented_inference(x, y, w)
                 loss = self.problem.loss(y, y_hat)
-
-                already_active = np.any([True for y_hat_, psi_, loss_ in
-                    constraints[i] if (y_hat == y_hat_).all()])
-
                 delta_psi = psi(x, y) - psi(x, y_hat)
                 slack = loss - np.dot(w, delta_psi)
+
                 if self.verbose > 1:
                     print("current slack: %f" % slack)
                 primal_objective += slack
+
+                already_active = np.any([True for y_hat_, psi_, loss_ in
+                    constraints[i] if (y_hat == y_hat_).all()])
+                if already_active:
+                    continue
 
                 if self.check_constraints:
                     # "smart" but expensive stopping criterion
@@ -144,6 +146,7 @@ class StructuredSVM(object):
             if self.verbose > 0:
                 print(w)
         self.w = w
+        print("calls to inference: %d" % self.problem.inference_calls)
         plt.figure()
         plt.subplot(131, title="loss")
         plt.plot(loss_curve)
@@ -226,6 +229,7 @@ class SubgradientStructuredSVM(StructuredSVM):
                 print(w)
         self.w = w
         print(objective_curve[-1])
+        print("calls to inference: %d" % self.problem.inference_calls)
         plt.subplot(121, title="loss")
         plt.plot(loss_curve[10:])
         plt.subplot(122, title="objective")