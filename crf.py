Fix annoying signs

crf.py
@@ -181,7 +181,7 @@ class MultinomialFixedGraphCRF(StructuredProblem):
                 - np.diag(np.diag(pairwise_params))
         unaries = (-1000 * unary_params * x).astype(np.int32)
         pairwise = (-1000 * pairwise_params).astype(np.int32)
-        y = alpha_expansion_graph(self.edges, unaries, pairwise, random_seed=10)
+        y = alpha_expansion_graph(self.edges, unaries, pairwise, random_seed=1)
         return y
 
     def loss_augmented_inference(self, x, y, w):
@@ -232,7 +232,7 @@ class MultinomialFixedGraphCRFNoBias(MultinomialGridCRF):
         pairwise_params[np.tri(self.n_states, k=-1, dtype=np.bool)] = pairwise_flat
         pairwise_params = pairwise_params + pairwise_params.T\
                 - np.diag(np.diag(pairwise_params))
-        unaries = (1000 * x).astype(np.int32)
+        unaries = (-1000 * x).astype(np.int32)
         pairwise = (-1000 * pairwise_params).astype(np.int32)
         y = alpha_expansion_graph(self.edges, unaries, pairwise, random_seed=1)
         return y

examples_latent_crf.py
@@ -20,7 +20,7 @@ def make_dataset_easy_latent(n_samples=5):
             t, l = np.random.randint(15, size=2)
             Y[i, t:t + 3, l:l + 3] = -1
     X = Y + 0.5 * np.random.normal(size=Y.shape)
-    X = np.c_['3,4,0', X, -X]
+    X = np.c_['3,4,0', -X, X]
     Y = (Y > 0).astype(np.int32)
     return X * 10, Y
 
@@ -45,7 +45,7 @@ def make_dataset_easy_latent_explicit(n_samples=5):
     #X = np.zeros((n_samples, 18, 18, 2))
     ix, iy, iz = np.ogrid[:X.shape[0], :X.shape[1], :X.shape[2]]
     #X[ix, iy, iz, Y_flips_2] = -1
-    X[ix, iy, iz, Y_flips] = -1
+    X[ix, iy, iz, Y_flips] = 1
     return X, Y
 
 

examples_multinomial_grid_crf.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from scipy import sparse
+from pyqpbo import alpha_expansion_graph
 
 #from crf import MultinomialFixedGraphCRFNoBias
 from crf import MultinomialFixedGraphCRFNoBias
@@ -9,8 +10,8 @@ from crf import MultinomialFixedGraphCRFNoBias
 #from structured_perceptron import StructuredPerceptron
 from structured_svm import StructuredSVM
 #SubgradientStructuredSVM
-from examples_latent_crf import make_dataset_easy_latent
-#from examples_latent_crf import make_dataset_easy_latent_explicit
+#from examples_latent_crf import make_dataset_easy_latent
+from examples_latent_crf import make_dataset_easy_latent_explicit
 
 
 from IPython.core.debugger import Tracer
@@ -70,8 +71,8 @@ def make_dataset_big_checker_extended():
 
 def main():
     #X, Y = make_dataset_checker_multinomial()
-    #X, Y = make_dataset_easy_latent_explicit(n_samples=1)
-    X, Y = make_dataset_easy_latent(n_samples=1)
+    X, Y = make_dataset_easy_latent_explicit(n_samples=1)
+    #X, Y = make_dataset_easy_latent(n_samples=1)
     #X, Y = make_dataset_big_checker_extended()
     #X, Y = make_dataset_big_checker()
     #X, Y = make_dataset_blocks_multinomial(n_samples=5)
@@ -111,7 +112,10 @@ def main():
             continue
         fig, plots = plt.subplots(1, 4)
         plots[0].imshow(y, interpolation='nearest')
-        plots[1].imshow(np.argmin(x, axis=2), interpolation='nearest')
+        pw_z = np.zeros((n_labels, n_labels), dtype=np.int32)
+        un = (-1000 * x.reshape(-1, n_labels)).astype(np.int32)
+        unaries = alpha_expansion_graph(edges, un, pw_z)
+        plots[1].imshow(unaries.reshape(y.shape), interpolation='nearest')
         plots[2].imshow(y_pred, interpolation='nearest')
         loss_augmented = clf.problem.loss_augmented_inference(
                 x.reshape(-1, n_labels), y.ravel(), clf.w)