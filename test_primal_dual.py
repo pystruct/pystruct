635999e996a5f1e05d6052ed3c06765cb005586e
Andy <amueller@ais.uni-bonn.de>
2012-06-29 16:45:23 +0200

    ENH testing with differnt Cs, cleanup

--- test_primal_dual.py
+++ test_primal_dual.py
@@ -1,4 +1,3 @@
-from numpy.testing import assert_array_equal
 from crf import BinaryGridCRF
 from structured_svm import StructuredSVM, SubgradientStructuredSVM
 from structured_svm import objective_primal
@@ -9,23 +8,19 @@ tracer = Tracer()
 
 
 def test_primal_dual_binary():
-    for dataset in binary:
-        X, Y = dataset(n_samples=1)
-        crf = BinaryGridCRF()
-        C = 1000000
-        clf = StructuredSVM(problem=crf, max_iter=200, C=C, verbose=0,
-                check_constraints=True)
-        clf.fit(X, Y)
-        clf2 = SubgradientStructuredSVM(problem=crf, max_iter=200, C=C,
-                verbose=0)
-        clf2.fit(X, Y)
-        obj = objective_primal(crf, clf.w, X, Y, C)
-        obj2 = objective_primal(crf, clf2.w, X, Y, C)
-        assert(obj <= obj2)
-        print("objective difference: %f" % (obj2 - obj))
-        print(clf.predict(X))
-        print(clf2.predict(X))
-        print("=" * 20)
-        #assert_array_equal(clf.predict(X), clf2.predict(X))
-
+    for C in [1, 100, 100000]:
+        for dataset in binary:
+            X, Y = dataset(n_samples=1)
+            crf = BinaryGridCRF()
+            clf = StructuredSVM(problem=crf, max_iter=200, C=C, verbose=0,
+                    check_constraints=True)
+            clf.fit(X, Y)
+            clf2 = SubgradientStructuredSVM(problem=crf, max_iter=200, C=C,
+                    verbose=0)
+            clf2.fit(X, Y)
+            obj = objective_primal(crf, clf.w, X, Y, C)
+            # the dual finds the optimum so it might be better
+            obj2 = objective_primal(crf, clf2.w, X, Y, C)
+            assert(obj <= obj2)
+            print("objective difference: %f\n" % (obj2 - obj))
 test_primal_dual_binary()
