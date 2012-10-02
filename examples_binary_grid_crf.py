trying to fix signs again. tests working, positivity constraint screwed?

examples_binary_grid_crf.py
@@ -13,7 +13,7 @@ tracer = Tracer()
 
 
 def main():
-    X, Y = generate_blocks(n_samples=1)
+    X, Y = generate_blocks(n_samples=10)
     #X, Y = generate_checker()
     #X, Y = generate_easy(n_samples=1)
     #X, Y = generate_big_checker()
@@ -21,7 +21,8 @@ def main():
     #clf = StructuredPerceptron(problem=crf, max_iter=100)
     clf = StructuredSVM(problem=crf, max_iter=200, C=100, verbose=10,
             check_constraints=True)
-    #clf = SubgradientStructuredSVM(problem=crf, max_iter=2000, C=100)
+    #clf = SubgradientStructuredSVM(problem=crf, max_iter=2000, C=100,
+            #verbose=10, momentum=.9)
     clf.fit(X, Y)
     Y_pred = clf.predict(X)
     i = 0
@@ -30,16 +31,16 @@ def main():
         loss += np.sum(y != y_pred)
         fig, plots = plt.subplots(2, 2)
         plots[0, 0].set_title("x")
-        plots[0, 0].imshow(x[:, :, 0], interpolation='nearest')
+        plots[0, 0].matshow(-x[:, :, 0])
         plots[0, 1].set_title("y")
-        plots[0, 1].imshow(y, interpolation='nearest')
+        plots[0, 1].matshow(y)
         plots[1, 0].set_title("unaries")
-        w_unaries = np.zeros(4)
+        w_unaries = np.zeros(2)
         w_unaries[0] = 1.
         y_unaries = crf.inference(x, w_unaries)
-        plots[1, 0].imshow(y_unaries, interpolation='nearest')
+        plots[1, 0].matshow(y_unaries)
         plots[1, 1].set_title("full crf")
-        plots[1, 1].imshow(y_pred, interpolation='nearest')
+        plots[1, 1].matshow(y_pred)
         for plot in plots.ravel():
             plot.set_axis_off()
         fig.savefig("data_%03d.png" % i)

examples_multinomial_grid_crf.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from scipy import sparse
-from pyqpbo import alpha_expansion_graph
-
 #from crf import MultinomialFixedGraphCRFNoBias
-from crf import MultinomialFixedGraphCRFNoBias
-#from crf import MultinomialGridCRF
+#from crf import MultinomialFixedGraphCRFNoBias
+from crf import MultinomialGridCRF
 #from structured_perceptron import StructuredPerceptron
 from structured_svm import StructuredSVM
-from toy_datasets import make_dataset_big_checker
+#from structured_svm import SubgradientStructuredSVM
+#from toy_datasets import generate_big_checker
+#from toy_datasets import generate_checker_multinomial
+from toy_datasets import generate_blocks_multinomial
 
 
 from IPython.core.debugger import Tracer
@@ -17,33 +17,19 @@ tracer = Tracer()
 
 
 def main():
-    X, Y = make_dataset_big_checker(n_samples=1)
-    size_y = Y[0].size
-    shape_y = Y[0].shape
+    #X, Y = generate_checker_multinomial(n_samples=10, noise=0.0)
+    X, Y = generate_blocks_multinomial(n_samples=10, noise=0.0)
     n_labels = len(np.unique(Y))
-    inds = np.arange(size_y).reshape(shape_y)
-    horz = np.c_[inds[:, :-1].ravel(), inds[:, 1:].ravel()]
-    vert = np.c_[inds[:-1, :].ravel(), inds[1:, :].ravel()]
-    downleft = np.c_[inds[:-1, :-1].ravel(), inds[1:, 1:].ravel()]
-    downright = np.c_[inds[:-1, 1:].ravel(), inds[1:, :-1].ravel()]
-    edges = np.vstack([horz, vert, downleft, downright]).astype(np.int32)
-    graph = sparse.coo_matrix((np.ones(edges.shape[0]),
-        (edges[:, 0], edges[:, 1])), shape=(size_y, size_y)).tocsr()
-    graph = graph + graph.T
-
-    crf = MultinomialFixedGraphCRFNoBias(n_states=n_labels, graph=graph)
-    #crf = MultinomialGridCRF(n_labels=4)
+    crf = MultinomialGridCRF(n_states=n_labels)
     #clf = StructuredPerceptron(problem=crf, max_iter=50)
-    clf = StructuredSVM(problem=crf, max_iter=20, C=1000000, verbose=20,
-            check_constraints=True,
-            positive_constraint=np.arange(crf.size_psi - 1))
-    #clf = SubgradientStructuredSVM(problem=crf, max_iter=100, C=10000)
-    X_flat = [x.reshape(-1, n_labels).copy("C") for x in X]
-    Y_flat = [y.ravel() for y in Y]
-    clf.fit(X_flat, Y_flat)
-    #clf.fit(X, Y)
-    Y_pred = clf.predict(X_flat)
-    #Y_pred = clf.predict(X)
+    clf = StructuredSVM(problem=crf, max_iter=20, C=100000, verbose=20,
+            check_constraints=True)
+            #positive_constraint=np.arange(crf.size_psi - 1))
+    #clf = SubgradientStructuredSVM(problem=crf, max_iter=100, C=10,
+            #verbose=10, momentum=.98, alpha=0.01)
+    clf.fit(X, Y)
+    Y_pred = clf.predict(X)
+    print(clf.w)
 
     i = 0
     loss = 0
@@ -51,24 +37,25 @@ def main():
         y_pred = y_pred.reshape(x.shape[:2])
         #loss += np.sum(y != y_pred)
         loss += np.sum(np.logical_xor(y, y_pred))
-        if i > 4:
+        if i > 10:
             continue
         fig, plots = plt.subplots(1, 4)
-        plots[0].imshow(y, interpolation='nearest')
+        plots[0].matshow(y)
         plots[0].set_title("gt")
-        pw_z = np.zeros((n_labels, n_labels), dtype=np.int32)
-        un = np.ascontiguousarray(
-                -1000 * x.reshape(-1, n_labels)).astype(np.int32)
-        unaries = alpha_expansion_graph(edges, un, pw_z)
-        plots[1].imshow(unaries.reshape(y.shape), interpolation='nearest')
+        w_unaries_only = np.zeros(crf.size_psi)
+        w_unaries_only[:n_labels] = 1.
+        unary_pred = crf.inference(x, w_unaries_only)
+        plots[1].matshow(unary_pred)
         plots[1].set_title("unaries only")
-        plots[2].imshow(y_pred, interpolation='nearest')
+        plots[2].matshow(y_pred)
         plots[2].set_title("prediction")
-        loss_augmented = clf.problem.loss_augmented_inference(
-                x.reshape(-1, n_labels), y.ravel(), clf.w)
+        loss_augmented = clf.problem.loss_augmented_inference(x, y, clf.w)
         loss_augmented = loss_augmented.reshape(y.shape)
-        plots[3].imshow(loss_augmented, interpolation='nearest')
+        plots[3].matshow(loss_augmented)
         plots[3].set_title("loss augmented")
+        for p in plots:
+            p.set_xticks(())
+            p.set_yticks(())
         fig.savefig("data_%03d.png" % i)
         plt.close(fig)
         i += 1

test_crfs.py
@@ -35,26 +35,10 @@ def test_binary_grid_unaries():
         except:
             print(ds)
 
-
-def test_multinomial_grid_binary():
-    # test handling on unaries for multinomial grid CRFs
-    # on binary datasets
-    for ds in toy_datasets.binary:
-        X, Y = ds(n_samples=1)
-        x, y = X[0], Y[0]
-        crf = MultinomialGridCRF()
-        w_unaries_only = np.zeros(5)
-        w_unaries_only[:2] = 1.
-        # test that inference with unaries only is the
-        # same as argmax
-        inf_unaries = crf.inference(x, w_unaries_only)
-
-        pw_z = np.zeros((2, 2), dtype=np.int32)
-        un = np.ascontiguousarray(
-                -1000 * x).astype(np.int32)
-        unaries = binary_grid(un, pw_z)
-        assert_array_equal(inf_unaries, unaries)
-        assert_array_equal(inf_unaries, np.argmax(x, axis=2))
+        # check that the right thing happens on noise-free data
+        X, Y = ds(n_samples=1, noise=0)
+        inf_unaries = crf.inference(X[0], w_unaries_only)
+        assert_array_equal(inf_unaries, Y[0])
 
 
 def test_multinomial_grid_unaries():
@@ -77,6 +61,10 @@ def test_multinomial_grid_unaries():
         unaries = alpha_expansion_grid(un, pw_z)
         assert_array_equal(inf_unaries, unaries)
         assert_array_equal(inf_unaries, np.argmax(x, axis=2))
+        # check that the right thing happens on noise-free data
+        X, Y = ds(n_samples=1, noise=0)
+        inf_unaries = crf.inference(X[0], w_unaries_only)
+        assert_array_equal(inf_unaries, Y[0])
 
 
 def exhausive_inference_binary(problem, x, w):
@@ -119,9 +107,9 @@ def test_binary_crf_exhaustive():
         # check map inference
         y_hat = crf.inference(x, w)
         y_ex = exhausive_inference_binary(crf, x, w)
-        print(y_hat)
-        print(y_ex)
-        print("++++++++++++++++++++++")
+        #print(y_hat)
+        #print(y_ex)
+        #print("++++++++++++++++++++++")
         assert_array_equal(y_hat, y_ex)
 
 
@@ -139,14 +127,13 @@ def test_binary_crf_exhaustive_loss_augmented():
         # check loss augmented map inference
         y_hat = crf.loss_augmented_inference(x, y, w)
         y_ex = exhausive_loss_augmented_inference_binary(crf, x, y, w)
-        print(y_hat)
-        print(y_ex)
-        print("++++++++++++++++++++++")
+        #print(y_hat)
+        #print(y_ex)
+        #print("++++++++++++++++++++++")
         assert_array_equal(y_hat, y_ex)
 
-
-test_binary_crf_exhaustive()
-test_binary_crf_exhaustive_loss_augmented()
-test_binary_grid_unaries()
-test_multinomial_grid_binary()
-test_multinomial_grid_unaries()
+if __name__ == "__main__":
+    test_binary_crf_exhaustive()
+    test_binary_crf_exhaustive_loss_augmented()
+    test_binary_grid_unaries()
+    test_multinomial_grid_unaries()

toy_datasets.py
@@ -2,76 +2,76 @@ import numpy as np
 
 
 #### binary
-def generate_blocks(n_samples=10):
+def generate_blocks(n_samples=10, noise=1.5):
     np.random.seed(0)
     Y = np.ones((n_samples, 10, 12))
     Y[:, :, :6] = -1
-    X = Y + 1.5 * np.random.normal(size=Y.shape)
+    X = Y + noise * np.random.normal(size=Y.shape)
     X = np.c_['3,4,0', -X, np.zeros_like(X)]
     Y = (Y > 0).astype(np.int32)
     return X, Y
 
 
-def generate_checker(n_samples=20):
+def generate_checker(n_samples=20, noise=1.5):
     np.random.seed(0)
     Y = np.ones((n_samples, 11, 13))
     Y[:, ::2, ::2] = -1
     Y[:, 1::2, 1::2] = -1
-    X = Y + 1.5 * np.random.normal(size=Y.shape)
+    X = Y + noise * np.random.normal(size=Y.shape)
     X = np.c_['3,4,0', -X, np.zeros_like(X)]
     Y = (Y > 0).astype(np.int32)
     return X, Y
 
 
-def generate_big_checker(n_samples=10):
+def generate_big_checker(n_samples=10, noise=0.5):
     np.random.seed(0)
     y_small = np.ones((11, 13), dtype=np.int32)
     y_small[::2, ::2] = -1
     y_small[1::2, 1::2] = -1
     y = y_small.repeat(3, axis=0).repeat(3, axis=1)
     Y = np.repeat(y[np.newaxis, :, :], n_samples, axis=0)
-    X = Y + 0.5 * np.random.normal(size=Y.shape)
+    X = Y + noise * np.random.normal(size=Y.shape)
     Y = (Y < 0).astype(np.int32)
     # make unaries with 4 pseudo-classes
     X = np.r_['-1, 4,0', X, np.zeros_like(X)].copy("C")
     return X, Y
 
 
-def generate_easy(n_samples=5):
+def generate_easy(n_samples=5, noise=0.5):
     np.random.seed(0)
     Y = np.ones((n_samples, 18, 18))
     for i in xrange(n_samples):
         for j in xrange(3):
             t, l = np.random.randint(15, size=2)
             Y[i, t:t + 3, l:l + 3] = -1
-    X = Y + 0.5 * np.random.normal(size=Y.shape)
+    X = Y + noise * np.random.normal(size=Y.shape)
     X = np.c_['3,4,0', -X, np.zeros_like(X)]
     Y = (Y > 0).astype(np.int32)
     return X * 10, Y
 
 
 #### Multinomial
-def generate_blocks_multinomial(n_samples=20):
+def generate_blocks_multinomial(n_samples=20, noise=0.5):
     Y = np.zeros((n_samples, 10, 12, 3))
-    Y[:, :, :4, 0] = -1
-    Y[:, :, 4:8, 1] = -1
-    Y[:, :, 8:16, 2] = -1
-    X = Y + 0.5 * np.random.normal(size=Y.shape)
-    Y = np.argmin(Y, axis=3).astype(np.int32)
+    Y[:, :, :4, 0] = 1
+    Y[:, :, 4:8, 1] = 1
+    Y[:, :, 8:16, 2] = 1
+    X = Y + noise * np.random.normal(size=Y.shape)
+    Y = np.argmax(Y, axis=3).astype(np.int32)
     return X, Y
 
 
-def generate_checker_multinomial(n_samples=20):
-    Y = np.ones((n_samples, 10, 12, 3))
-    Y[:, ::2, ::2, 0] = -1
-    Y[:, 1::2, 1::2, 1] = -1
+def generate_checker_multinomial(n_samples=20, noise=1.5):
+    Y = -np.ones((n_samples, 10, 12, 3))
+    Y[:, ::2, ::2, 0] = 1
+    Y[:, 1::2, 1::2, 1] = 1
     Y[:, :, :, 2] = 0
-    X = Y + 1.5 * np.random.normal(size=Y.shape)
-    Y = np.argmin(Y, axis=3).astype(np.int32)
+    X = Y + noise * np.random.normal(size=Y.shape)
+    Y = np.argmax(Y, axis=3).astype(np.int32)
     return X, Y
 
 
-def generate_big_checker_extended(n_samples=20):
+def generate_big_checker_extended(n_samples=20, noise=.3):
     y_small = np.zeros((6, 6), dtype=np.int32)
     y_small[::2, ::2] = 2
     y_small[1::2, 1::2] = 2
@@ -84,12 +84,12 @@ def generate_big_checker_extended(n_samples=20):
     X_shape.append(4)
     X = np.zeros(X_shape)
     gx, gy, gz = np.mgrid[:Y.shape[0], :Y.shape[1], :Y.shape[2]]
-    X[gx, gy, gz, Y] = -1
-    X = X + 0.3 * np.random.normal(size=X.shape)
+    X[gx, gy, gz, Y] = 1
+    X = X + noise * np.random.normal(size=X.shape)
     return X * 100., Y
 
 
-def generate_easy_explicit(n_samples=5):
+def generate_easy_explicit(n_samples=5, noise=30):
     np.random.seed(0)
     Y = np.zeros((n_samples, 18, 18), dtype=np.int)
     for i in xrange(n_samples):
@@ -99,10 +99,9 @@ def generate_easy_explicit(n_samples=5):
             Y[i, t + 1, l + 1] = 2
     Y_flips = Y.copy()
     #flip random bits
-    n_flips = 30
     for y in Y_flips:
-        flips = np.random.randint(18, size=[n_flips, 2])
-        y[flips[:, 0], flips[:, 1]] = np.random.randint(3, size=n_flips)
+        flips = np.random.randint(18, size=[noise, 2])
+        y[flips[:, 0], flips[:, 1]] = np.random.randint(3, size=noise)
     #Y = (Y != 0).astype(np.int)
     #Y_flips_2 = (Y_flips != 0).astype(np.int)
     X = np.zeros((n_samples, 18, 18, 3))