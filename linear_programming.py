75a83233cf6bdabc9ac59b7fb08a326680d3542f
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-11-08 12:28:46 +0100

    FIX might work ^^. remove last row from A for full rank

--- linear_programming.py
+++ linear_programming.py
@@ -1,7 +1,6 @@
-import toy_datasets as toy
 import numpy as np
 import scipy.sparse as sp
-
+from cvxopt import matrix, solvers
 
 from IPython.core.debugger import Tracer
 tracer = Tracer()
@@ -20,7 +19,9 @@ def grid_linear_programming(x, weights):
     n_states = x.shape[2]
 
     # unaries are stored as "first all state0, then all state1"
-    unary_potentials = x.ravel()
+    unary_potentials = np.rollaxis(x, 2, 0).ravel()
+    tracer()
+
     # pairwise potentials
     # first all right, then all down
     # first all of state 00, then 01, 10, 11
@@ -32,15 +33,17 @@ def grid_linear_programming(x, weights):
                       column_penalty, column_zeros])
     row_zeros = np.zeros(height * (width - 1))
     row_penalty = np.repeat(pw, height * (width - 1))
-    right = np.hstack([row_zeros, row_penalty,
-                       row_penalty, row_zeros])
+    right = np.hstack([row_zeros, row_penalty, row_penalty, row_zeros])
 
     #together:
-    c = np.hstack([unary_potentials, down, right])
+    #c = np.hstack([unary_potentials, down, right])
+    c = np.hstack([unary_potentials, np.repeat(100, n_edges * n_states ** 2)])
 
     # non-negativity constraints for all variables
     G = -np.eye(n_vars)
     h = np.zeros(n_vars)
+    #G = -np.eye(n_nodes * n_states)
+    #h = np.zeros(n_nodes * n_states)
 
     # summation constraints for unaries: state variables have to sum to one
     Aun = np.hstack(np.repeat([np.eye(n_nodes)], n_states, axis=0))
@@ -106,17 +109,32 @@ def grid_linear_programming(x, weights):
 
     A = sp.vstack([Aun, Apw])
     b = np.hstack([bun, bpw])
+    #A = Aun
+    #b = bun
+    c_ = matrix(c)
+    G_ = matrix(G)
+    h_ = matrix(h)
+    A_ = matrix(A.toarray()[:-1, :])
+    #A_ = matrix(A)
+    b_ = matrix(b[:-1])
+    solvers.options['feastol'] = 1e-3
+    #sol = solvers.lp(c_, G_, h_, A_, b_)
+    sol = solvers.lp(c_, G_, h_, A_, b_)
+    result = np.asarray(sol['x'])
     tracer()
-    [c, G, h, A, b]
-
-    #bpw = np.zeros(n_nodes)
+    node_vars = result[:n_nodes * n_states]
+    node_vars = node_vars.reshape(n_states, width, height)
+    edge_vars = result[n_nodes * n_states:]
+    print(result)
 
 
 def main():
-    X, Y = toy.generate_blocks(n_samples=10)
-    x, y = X[0], Y[0]
     weights = np.array([1, -2])
-    grid_linear_programming(x, weights)
+    Y = np.ones((3, 4))
+    Y[:, :2] = -1
+    X = np.c_['2,3,0', -Y, np.zeros_like(Y)]
+    Y = (Y > 0).astype(np.int32)
+    grid_linear_programming(X, weights)
 
 
 if __name__ == "__main__":
