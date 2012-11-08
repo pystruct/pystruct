7b3b2baf8ea01227cb830bc2cb63540010c758bd
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-11-08 18:38:00 +0100

    work please, maybe?

--- linear_programming.py
+++ linear_programming.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.sparse as sp
 from cvxopt import matrix, solvers
+import matplotlib.pyplot as plt
 
 from IPython.core.debugger import Tracer
 tracer = Tracer()
@@ -18,25 +19,20 @@ def grid_linear_programming(x, weights):
     n_nodes = width * height
     n_states = x.shape[2]
 
-    # unaries are stored as "first all state0, then all state1"
-    unary_potentials = np.rollaxis(x, 2, 0).ravel()
-    tracer()
+    # unaries are stored as node_0: state_0, state_1
+    unary_potentials = x.ravel()
 
     # pairwise potentials
     # first all right, then all down
-    # first all of state 00, then 01, 10, 11
+    # edge01, state 00, 01, 10, 11, edge12,...
+    pw = np.array([[0, pw], [pw, 0]])
+
     n_edges = width * (height - 1) + height * (width - 1)
     n_vars = n_states * n_nodes + n_states ** 2 * n_edges
-    column_zeros = np.zeros(width * (height - 1))
-    column_penalty = np.repeat(pw, width * (height - 1))
-    down = np.hstack([column_zeros, column_penalty,
-                      column_penalty, column_zeros])
-    row_zeros = np.zeros(height * (width - 1))
-    row_penalty = np.repeat(pw, height * (width - 1))
-    right = np.hstack([row_zeros, row_penalty, row_penalty, row_zeros])
+    edge_penalties = np.repeat(pw, n_edges).ravel()
 
     #together:
-    #c = np.hstack([unary_potentials, down, right])
+    #c = np.hstack([unary_potentials, edge_penalties])
     c = np.hstack([unary_potentials, np.repeat(100, n_edges * n_states ** 2)])
 
     # non-negativity constraints for all variables
@@ -52,59 +48,71 @@ def grid_linear_programming(x, weights):
     # add loads of zeros for pairwise variables
     bun = np.ones(n_nodes)
 
-    # summation constraint for pairwise: have to sum to unary
-    # build incidence matrix (which vertex is part of which edge)
-    # for all nodes first the zero state, then the one state
-    node_states = -np.eye(n_nodes * n_states)
-    # for the edges to the right
-    # create a block matrix
-    #
-    # un_0_pw_00, un_0_pw01, un_0_pw_10, un_0_pw_11
-    # un_1_pw_00, un_1_pw01, un_1_pw_10, un_1_pw_11
+    # summation constraint for pairwise: summing out one
+    # state has to yield unary for the other one.
+    # leading to 2 * n_edges  * n_states constraints
+    constraints = np.zeros((2 * n_edges * n_states,
+            n_states * n_nodes + n_states ** 2 * n_edges))
+    # edges to the right
+    # off is offset for edge variables, i.e. number of node variables
+    off = n_nodes * n_states
+    i = 0
+    for row in xrange(height):
+        for variable in xrange(width - 1):
+            for state in xrange(n_states):
+                # index of variable:
+                constraints[i, n_states * (row * width + variable) + state] = -1
+                # select edge with this node as left variable
+                edge_ind = off + n_states ** 2 * (row * (width - 1) + variable)
+                # states are sorted lexically
+                for other_state in xrange(n_states):
+                    constraints[i, edge_ind + state * n_states + other_state] = +1
+                i += 1
+    # edges to the left
+    for row in xrange(height):
+        for variable in xrange(width - 1):
+            for state in xrange(n_states):
+                # index of variable:
+                constraints[i, n_states * (row * width + variable + 1) + state] = -1
+                # select edge with this node as left variable
+                edge_ind = off + n_states ** 2 * (row * (width - 1) + variable)
+                # states are sorted lexically
+                for other_state in xrange(n_states):
+                    constraints[i, edge_ind + other_state * n_states + state] = +1
+                i += 1
+    # edges down
+    # shift by number of horizontal edges
+    off += height * (width - 1) * n_states ** 2
+    for row in xrange(height - 1):
+        for variable in xrange(width):
+            for state in xrange(n_states):
+                # index of variable:
+                constraints[i, n_states * (row * width + variable) + state] = -1
+                # select edge with this node as left variable
+                edge_ind = off + n_states ** 2 * (row * width + variable)
+                # states are sorted lexically
+                for other_state in xrange(n_states):
+                    constraints[i, edge_ind + state * n_states + other_state] = +1
+                i += 1
+    # edges up
+    for row in xrange(height - 1):
+        for variable in xrange(width):
+            for state in xrange(n_states):
+                # index of variable:
+                constraints[i, n_states * ((row + 1) * width + variable) + state] = -1
+                # select edge with this node as left variable
+                edge_ind = off + n_states ** 2 * (row * width + variable)
+                # states are sorted lexically
+                for other_state in xrange(n_states):
+                    constraints[i, edge_ind + other_state * n_states + state] = +1
+                i += 1
+    tracer()
 
-    un_0_pw_00 = sp.block_diag([np.eye(width, width - 1)
-                              + np.eye(width, width - 1, -1)] * height)
-    un_1_pw_00 = np.zeros((n_nodes, height * (width - 1)))
-    pw_00 = sp.vstack([un_0_pw_00, un_1_pw_00])
 
-    un_0_pw_01 = sp.block_diag([np.eye(width, width - 1)] * height)
-    un_1_pw_01 = sp.block_diag([np.eye(width, width - 1, -1)] * height)
-    pw_01 = sp.vstack([un_0_pw_01, un_1_pw_01])
 
-    un_0_pw_10 = sp.block_diag([np.eye(width, width - 1, -1)] * height)
-    un_1_pw_10 = sp.block_diag([np.eye(width, width - 1)] * height)
-    pw_10 = sp.vstack([un_0_pw_10, un_1_pw_10])
-
-    un_0_pw_11 = np.zeros((n_nodes, height * (width - 1)))
-    un_1_pw_11 = sp.block_diag([np.eye(width, width - 1)
-                              + np.eye(width, width - 1, -1)] * height)
-    pw_11 = sp.vstack([un_0_pw_11, un_1_pw_11])
-
-    to_right = sp.hstack([pw_00, pw_01, pw_10, pw_11])
-
-    # same for the edgeds down
-    un_0_pw_00 = sp.block_diag([np.eye(height, height - 1)
-                              + np.eye(height, height - 1, -1)] * width)
-    un_1_pw_00 = np.zeros((n_nodes, width * (height - 1)))
-    pw_00 = sp.vstack([un_0_pw_00, un_1_pw_00])
-
-    un_0_pw_01 = sp.block_diag([np.eye(height, height - 1)] * width)
-    un_1_pw_01 = sp.block_diag([np.eye(height, height - 1, -1)] * width)
-    pw_01 = sp.vstack([un_0_pw_01, un_1_pw_01])
-
-    un_0_pw_10 = sp.block_diag([np.eye(height, height - 1, -1)] * width)
-    un_1_pw_10 = sp.block_diag([np.eye(height, height - 1)] * width)
-    pw_10 = sp.vstack([un_0_pw_10, un_1_pw_10])
-
-    un_0_pw_11 = np.zeros((n_nodes, width * (height - 1)))
-    un_1_pw_11 = sp.block_diag([np.eye(height, height - 1)
-                              + np.eye(height, height - 1, -1)] * width)
-    pw_11 = sp.vstack([un_0_pw_11, un_1_pw_11])
-
-    down = sp.hstack([pw_00, pw_01, pw_10, pw_11])
-
+    edge_consistency = np.eye(n_nodes * n_states)
     # subtract node state from sum of pairwise states
-    Apw = sp.hstack([node_states, to_right, down])
+    Apw = sp.hstack([node_states, edge_consistency])
     bpw = np.zeros(n_nodes * n_states)
 
     A = sp.vstack([Aun, Apw])
@@ -114,17 +122,30 @@ def grid_linear_programming(x, weights):
     c_ = matrix(c)
     G_ = matrix(G)
     h_ = matrix(h)
-    A_ = matrix(A.toarray()[:-1, :])
-    #A_ = matrix(A)
-    b_ = matrix(b[:-1])
+    # we need to remove the last row to make A full rank
+    #A_ = matrix(A.toarray()[:-1, :])
+    #b_ = matrix(b[:-1])
+    A_ = matrix(A.toarray())
+    b_ = matrix(b)
     solvers.options['feastol'] = 1e-3
     #sol = solvers.lp(c_, G_, h_, A_, b_)
     sol = solvers.lp(c_, G_, h_, A_, b_)
     result = np.asarray(sol['x'])
-    tracer()
     node_vars = result[:n_nodes * n_states]
     node_vars = node_vars.reshape(n_states, width, height)
     edge_vars = result[n_nodes * n_states:]
+    right_edges = edge_vars[: (width - 1) * height * n_states ** 2]
+    right_edges = right_edges.reshape(n_states ** 2, width - 1, height)
+    down_edges = edge_vars[(width - 1) * height * n_states ** 2:]
+    down_edges = down_edges.reshape(n_states ** 2, height - 1, width)
+    plt.matshow(node_vars[0], vmin=0, vmax=1)
+    plt.matshow(down_edges[0], vmin=0, vmax=1)
+    plt.matshow(down_edges[1], vmin=0, vmax=1)
+    plt.matshow(down_edges[2], vmin=0, vmax=1)
+    plt.matshow(down_edges[3], vmin=0, vmax=1)
+
+    plt.show()
+    tracer()
     print(result)
 
 
@@ -132,7 +153,9 @@ def main():
     weights = np.array([1, -2])
     Y = np.ones((3, 4))
     Y[:, :2] = -1
-    X = np.c_['2,3,0', -Y, np.zeros_like(Y)]
+    #X = Y + 1. * np.random.normal(size=Y.shape)
+    X = Y
+    X = np.c_['2,3,0', -X, np.zeros_like(X)]
     Y = (Y > 0).astype(np.int32)
     grid_linear_programming(X, weights)
 
