1ae958b0472b1bd7236c0824d722899cbd5bfe7d
Andy <amueller@ais.uni-bonn.de>
2012-11-12 17:41:35 +0100

    also junction tree inference for binary problems

--- crf.py
+++ crf.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-from pyqpbo import binary_grid
-from pyqpbo import alpha_expansion_grid, alpha_expansion_graph
+#from pyqpbo import binary_grid
+#from pyqpbo import alpha_expansion_grid, 
+from pyqpbo import alpha_expansion_graph
 from daimrf import mrf
 
 
@@ -60,21 +61,43 @@ class BinaryGridCRF(StructuredProblem):
         #pw = np.zeros((2, 2))
         return np.array([unaries_acc, pw[0, 1]])
 
+
     def inference(self, x, w):
+        self.inference_calls += 1
         if w.shape != (self.size_psi,):
             raise ValueError("Got w of wrong shape. Expected %s, got %s" %
                     (self.size_psi, w.shape))
-        self.inference_calls += 1
         unary_param = w[0]
         pairwise_params = np.array([[0, w[1]], [w[1], 0]])
         if (x[:, :, 1] != 0).any():
             raise ValueError("For simplicity, in binary CRFS,"
                     "all entries in the second feature should be 0.")
-
-        unaries = - 1000 * unary_param * x.copy()
-        pairwise = -1000 * pairwise_params
-        y = binary_grid(unaries.astype(np.int32), pairwise.astype(np.int32))
+        #build graph
+        inds = np.arange(x.shape[0] * x.shape[1]).reshape(x.shape[:2]).astype(np.int64)
+        horz = np.c_[inds[:, :-1].ravel(), inds[:, 1:].ravel()]
+        vert = np.c_[inds[:-1, :].ravel(), inds[1:, :].ravel()]
+        edges = np.vstack([horz, vert])
+        x = np.minimum(x, 100)
+        unaries = np.exp(unary_param * x.reshape(-1, self.n_states))
+        y = mrf(unaries, edges, np.exp(pairwise_params))
+        y = y.reshape(x.shape[0], x.shape[1])
         return y
+
+    #def inference(self, x, w):
+        #if w.shape != (self.size_psi,):
+            #raise ValueError("Got w of wrong shape. Expected %s, got %s" %
+                    #(self.size_psi, w.shape))
+        #self.inference_calls += 1
+        #unary_param = w[0]
+        #pairwise_params = np.array([[0, w[1]], [w[1], 0]])
+        #if (x[:, :, 1] != 0).any():
+            #raise ValueError("For simplicity, in binary CRFS,"
+                    #"all entries in the second feature should be 0.")
+
+        #unaries = - 1000 * unary_param * x.copy()
+        #pairwise = -1000 * pairwise_params
+        #y = binary_grid(unaries.astype(np.int32), pairwise.astype(np.int32))
+        #return y
 
     def loss_augmented_inference(self, x, y, w):
         if w.shape != (self.size_psi,):
