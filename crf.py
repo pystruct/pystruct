fix signs in lp inference...

crf.py
@@ -118,11 +118,9 @@ class GridCRF(StructuredProblem):
         vert = np.c_[inds[:-1, :].ravel(), inds[1:, :].ravel()]
         edges = np.vstack([horz, vert])
         unaries = unary_params * x.reshape(-1, self.n_states)
-
-        y = solve_lp(unaries, edges, pairwise_params)
+        y = solve_lp(-unaries, edges, -pairwise_params)
         y = np.argmax(y, axis=-1)
         y = y.reshape(x.shape[0], x.shape[1])
-
         return y
 
     def loss_augmented_inference(self, x, y, w):

lp_new.py
@@ -87,8 +87,8 @@ def solve_lp(unaries, edges, pairwise):
     lp.obj[:] = coef.tolist()   # Set objective coefficients
     lp.matrix = matrix
     lp.simplex()           # Solve this LP with the simplex method
-    print 'Z = %g;' % lp.obj.value,  # Retrieve and print obj func value
-    print '; '.join('%s = %g' % (c.name, c.primal) for c in lp.cols)
+    #print 'Z = %g;' % lp.obj.value,  # Retrieve and print obj func value
+    #print '; '.join('%s = %g' % (c.name, c.primal) for c in lp.cols)
     res = np.array([c.primal for c in lp.cols])
     unary_variables = res[:n_nodes * n_states].reshape(n_nodes, n_states)
     pairwise_variables = res[n_nodes * n_states:].reshape(n_edges,