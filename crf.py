Fix energy computation for continuous y. We need to take the edge marginals from the lp. still work to do in psi.

crf.py
@@ -36,7 +36,8 @@ def _inference_dai(x, unary_params, pairwise_params):
     return y
 
 
-def _inference_lp(x, unary_params, pairwise_params, relaxed=False):
+def _inference_lp(x, unary_params, pairwise_params, relaxed=False,
+                  return_energy=False, exact=True):
     n_states = x.shape[-1]
     ## build graph
     inds = np.arange(x.shape[0] * x.shape[1]).reshape(x.shape[:2])
@@ -45,8 +46,9 @@ def _inference_lp(x, unary_params, pairwise_params, relaxed=False):
     vert = np.c_[inds[:-1, :].ravel(), inds[1:, :].ravel()]
     edges = np.vstack([horz, vert])
     unaries = unary_params * x.reshape(-1, n_states)
-    y = solve_lp(-unaries, edges, -pairwise_params, exact=True)
-    n_fractional = np.sum(y.max(axis=-1) < .9)
+    res = solve_lp(-unaries, edges, -pairwise_params, exact=exact)
+    unary_marginals, pairwise_marginals, energy = res
+    n_fractional = np.sum(unary_marginals.max(axis=-1) < .99)
     #if n_fractional:
         #print("got fractional solution. trying again, this time exactly")
         #y = solve_lp(-unaries, edges, -pairwise_params, exact=True)
@@ -54,15 +56,27 @@ def _inference_lp(x, unary_params, pairwise_params, relaxed=False):
     if n_fractional:
         print("fractional solutions found: %d" % n_fractional)
     if relaxed:
-        return y.reshape(x.shape)
-    y = np.argmax(y, axis=-1)
-    y = y.reshape(x.shape[0], x.shape[1])
+        #horz_marginals = pairwise_marginals[:x.shape[0] * (x.shape[1] - 1), :]
+        #vert_marginals = pairwise_marginals[x.shape[0] * (x.shape[1] - 1):, :]
+        #horz_marginals = horz_marginals.reshape(x.shape[0], x.shape[1] - 1,
+                                                #x.shape[-1] ** 2)
+        #vert_marginals = vert_marginals.reshape(x.shape[0] - 1, x.shape[1],
+                                                #x.shape[-1] ** 2)
+        pairwise_accumulated = pairwise_marginals.sum(axis=0)
+        pairwise_accumulated = pairwise_accumulated.reshape(x.shape[-1],
+                                                            x.shape[-1])
+        y = (unary_marginals, pairwise_accumulated)
+    else:
+        y = np.argmax(unary_marginals, axis=-1)
+        y = y.reshape(x.shape[0], x.shape[1])
+    if return_energy:
+        return y, energy
     return y
 
 
-def _inference_ad3(x, unary_params, pairwise_params, relaxed=False):
+def _inference_ad3(x, unary_params, pairwise_params, relaxed=False, verbose=0):
     ## build graph
-    y = AD3.simple_grid(unary_params * x, pairwise_params)
+    y = AD3.simple_grid(unary_params * x, pairwise_params, verbose=verbose)
     n_fractional = np.sum(y.max(axis=-1) < .9)
     if n_fractional:
         print("fractional solutions found: %d" % n_fractional)
@@ -134,6 +148,7 @@ class GridCRF(CRF):
         # x is unaries
         # y is a labeling
         ## y can also be continuous (from lp)
+        tracer()
         if x.shape == y.shape:
             x_flat = x.reshape(-1, x.shape[-1])
             y_flat = y.reshape(-1, y.shape[-1])

lp_new.py
@@ -99,7 +99,7 @@ def solve_lp(unaries, edges, pairwise, exact=False):
                                                           n_states ** 2)
     assert((np.abs(unary_variables.sum(axis=1) - 1) < 1e-4).all())
     assert((np.abs(pairwise_variables.sum(axis=1) - 1) < 1e-4).all())
-    return unary_variables
+    return unary_variables, pairwise_variables, lp.obj.value
 
 
 def main():

structured_svm.py
@@ -7,7 +7,6 @@
 # and Structures Output Spaces
 
 import numpy as np
-#from numpy.testing import assert_almost_equal
 import cvxopt
 import cvxopt.solvers
 import matplotlib.pyplot as plt
@@ -19,6 +18,43 @@ from crf import GridCRF
 
 from IPython.core.debugger import Tracer
 tracer = Tracer()
+
+
+def compute_energy(x, y, unary_params, pairwise_params):
+    # x is unaries
+    # y is a labeling
+    n_states = x.shape[-1]
+    if isinstance(y, tuple):
+        # y can also be continuous (from lp)
+        # in this case, it comes with accumulated edge marginals
+        y, pw = y
+        x_flat = x.reshape(-1, x.shape[-1])
+        y_flat = y.reshape(-1, y.shape[-1])
+        unaries_acc = np.sum(x_flat * y_flat, axis=0)
+    else:
+        ## unary features:
+        gx, gy = np.ogrid[:x.shape[0], :x.shape[1]]
+        selected_unaries = x[gx, gy, y]
+        unaries_acc = np.bincount(y.ravel(), selected_unaries.ravel(),
+                                  minlength=n_states)
+
+        ##accumulated pairwise
+        #make one hot encoding
+        labels = np.zeros((y.shape[0], y.shape[1], n_states),
+                          dtype=np.int)
+        labels[gx, gy, y] = 1
+
+        # vertical edges
+        vert = np.dot(labels[1:, :, :].reshape(-1, n_states).T,
+                      labels[:-1, :, :].reshape(-1, n_states))
+        # horizontal edges
+        horz = np.dot(labels[:, 1:, :].reshape(-1, n_states).T,
+                      labels[:, :-1, :].reshape(-1, n_states))
+        pw = vert + horz
+    pw = pw + pw.T - np.diag(np.diag(pw))
+    energy = (np.dot(unaries_acc, unary_params)
+              + np.dot(np.tril(pw).ravel(), pairwise_params.ravel()))
+    return energy
 
 
 ## global functions for easy parallelization