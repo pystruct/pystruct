dac681efd6fac11166f6e2e429c44ac0f668fa79
Andy <amueller@ais.uni-bonn.de>
2012-12-03 11:11:34 +0100

    TST tests for perceptron

---
+++ tests/test_perceptron.py
@@ -0,0 +1,23 @@
+#import numpy as np
+from numpy.testing import assert_array_equal
+from pystruct.crf import GridCRF
+from pystruct.structured_perceptron import StructuredPerceptron
+import pystruct.toy_datasets as toy
+
+
+def test_binary_blocks():
+    X, Y = toy.generate_blocks(n_samples=10)
+    crf = GridCRF()
+    clf = StructuredPerceptron(problem=crf, max_iter=30)
+    clf.fit(X, Y)
+    Y_pred = clf.predict(X)
+    assert_array_equal(Y, Y_pred)
+
+
+def test_multinomial_blocks():
+    X, Y = toy.generate_blocks_multinomial(n_samples=10, noise=0.3, seed=0)
+    crf = GridCRF(n_states=X.shape[-1])
+    clf = StructuredPerceptron(problem=crf, max_iter=10)
+    clf.fit(X, Y)
+    Y_pred = clf.predict(X)
+    assert_array_equal(Y, Y_pred)
