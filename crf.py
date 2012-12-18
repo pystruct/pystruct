fix / test energy computation

crf.py
@@ -286,7 +286,7 @@ class DirectionalGridCRF(CRF):
         feature = np.hstack([unaries_acc, pw.ravel()])
         return feature
 
-    def inference(self, x, w, relaxed=False):
+    def inference(self, x, w, relaxed=False, return_energy=False):
         self.inference_calls += 1
         # extract unary weights
         unary_params = self.get_unary_weights(w)
@@ -308,8 +308,8 @@ class DirectionalGridCRF(CRF):
             #return _inference_dai(x, unary_params, pairwise_params,
                                   #self.neighborhood)
         if self.inference_method == "lp":
-            return _inference_lp(x, unary_params, edge_weights,
-                                 edges, relaxed)
+            return _inference_lp(x, unary_params, edge_weights, edges, relaxed,
+                                 return_energy=return_energy)
         #elif self.inference_method == "ad3":
             #return _inference_ad3(x, unary_params, pairwise_params,
                                   #self.neighborhood, relaxed)

tests/test_directional_crf.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from numpy.testing import assert_array_equal, assert_array_almost_equal
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_almost_equal
 
 import pystruct.toy_datasets as toy
 from pystruct.lp_new import lp_general_graph
@@ -101,3 +101,23 @@ def test_psi_continuous():
     xx, yy = np.indices(y.shape)
     assert_array_almost_equal(unary_psi,
                               np.bincount(y.ravel(), x[xx, yy, y].ravel()))
+
+
+def test_energy():
+    # make sure that energy as computed by ssvm is the same as by lp
+    np.random.seed(0)
+    found_fractional = False
+    crf = DirectionalGridCRF(n_states=3, inference_method='lp')
+    while not found_fractional:
+        x = np.random.normal(size=(2, 2, 3))
+        unary_params = np.ones(3)
+        pw1 = np.random.normal() * np.eye(3)
+        pw2 = np.random.normal() * np.eye(3)
+        w = np.hstack([unary_params, pw1.ravel(), pw2.ravel()])
+        res, energy = crf.inference(x, w, relaxed=True, return_energy=True)
+        found_fractional = np.any(np.max(res[0], axis=-1) != 1)
+
+        psi = crf.psi(x, res)
+        energy_svm = np.dot(psi, w)
+
+        assert_almost_equal(energy, -energy_svm)