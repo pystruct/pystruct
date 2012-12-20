f6e796a302ee785e5d7de1f0cfbf675a44904b0f
Andy <amueller@ais.uni-bonn.de>
2012-12-20 19:09:52 +0100

    ENH test multinomial and directional SSVM training

--- tests/test_multinomial_grid.py
+++ tests/test_multinomial_grid.py
@@ -10,12 +10,13 @@ def test_multinomial_blocks_cutting_plane():
     X, Y = toy.generate_blocks_multinomial(n_samples=10, noise=0.3,
                                            seed=0)
     n_labels = len(np.unique(Y))
-    crf = GridCRF(n_states=n_labels)
-    clf = StructuredSVM(problem=crf, max_iter=10, C=100, verbose=0,
-                        check_constraints=False)
-    clf.fit(X, Y)
-    Y_pred = clf.predict(X)
-    assert_array_equal(Y, Y_pred)
+    for inference_method in ['lp', 'qpbo', 'ad3']:
+        crf = GridCRF(n_states=n_labels, inference_method=inference_method)
+        clf = StructuredSVM(problem=crf, max_iter=10, C=100, verbose=0,
+                            check_constraints=False)
+        clf.fit(X, Y)
+        Y_pred = clf.predict(X)
+        assert_array_equal(Y, Y_pred)
 
 
 def test_multinomial_blocks_directional():
@@ -24,12 +25,14 @@ def test_multinomial_blocks_directional():
     X, Y = toy.generate_blocks_multinomial(n_samples=10, noise=0.3,
                                            seed=0)
     n_labels = len(np.unique(Y))
-    crf = DirectionalGridCRF(n_states=n_labels)
-    clf = StructuredSVM(problem=crf, max_iter=10, C=100, verbose=0,
-                        check_constraints=False)
-    clf.fit(X, Y)
-    Y_pred = clf.predict(X)
-    assert_array_equal(Y, Y_pred)
+    for inference_method in ['lp', 'ad3']:
+        crf = DirectionalGridCRF(n_states=n_labels,
+                                 inference_method=inference_method)
+        clf = StructuredSVM(problem=crf, max_iter=10, C=100, verbose=0,
+                            check_constraints=False)
+        clf.fit(X, Y)
+        Y_pred = clf.predict(X)
+        assert_array_equal(Y, Y_pred)
 
 
 def test_multinomial_blocks_subgradient():
