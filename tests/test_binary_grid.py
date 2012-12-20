0797c98f1f7bcbc24de5347efa1a2cd2b6aa5afd
Andy <amueller@ais.uni-bonn.de>
2012-12-20 19:08:23 +0100

    Test binary grid training on more inference methods

--- tests/test_binary_grid.py
+++ tests/test_binary_grid.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy.testing import assert_array_equal
+from nose.tools import assert_true
 from pystruct.crf import GridCRF
 from pystruct.structured_svm import StructuredSVM, SubgradientStructuredSVM
 import pystruct.toy_datasets as toy
@@ -7,8 +8,8 @@ import pystruct.toy_datasets as toy
 
 def test_binary_blocks_cutting_plane():
     #testing cutting plane ssvm on easy binary dataset
-    #for inference_method in ["dai", "lp", "qpbo", "ad3"]:
-    for inference_method in ["dai", "lp", "qpbo"]:
+    for inference_method in ["dai", "lp", "qpbo", "ad3"]:
+    #for inference_method in ["dai", "lp", "qpbo"]:
         X, Y = toy.generate_blocks(n_samples=5)
         crf = GridCRF(inference_method=inference_method)
         clf = StructuredSVM(problem=crf, max_iter=20, C=100, verbose=0,
@@ -45,23 +46,24 @@ def test_binary_ssvm_repellent_potentials():
     # test non-submodular learning with and without positivity constraint
     # dataset is checkerboard
     X, Y = toy.generate_checker()
-    crf = GridCRF()
-    clf = StructuredSVM(problem=crf, max_iter=200, C=100,
-                        verbose=0, check_constraints=True)
-    clf.fit(X, Y)
-    Y_pred = clf.predict(X)
-    # standard crf can predict perfectly
-    assert_array_equal(Y, Y_pred)
+    for inference_method in ["lp", "qpbo", "ad3"]:
+        crf = GridCRF(inference_method=inference_method)
+        clf = StructuredSVM(problem=crf, max_iter=200, C=100,
+                            verbose=0, check_constraints=True)
+        clf.fit(X, Y)
+        Y_pred = clf.predict(X)
+        # standard crf can predict perfectly
+        assert_array_equal(Y, Y_pred)
 
-    submodular_clf = StructuredSVM(problem=crf, max_iter=200, C=100,
-                                   verbose=0, check_constraints=True,
-                                   positive_constraint=[2, 3, 4])
-    submodular_clf.fit(X, Y)
-    Y_pred = submodular_clf.predict(X)
-    # submodular crf can not do better than unaries
-    for i, x in enumerate(X):
-        y_pred_unaries = crf.inference(x, np.array([1, 1, 0, 0, 0]))
-        assert_array_equal(y_pred_unaries, Y_pred[i])
+        submodular_clf = StructuredSVM(problem=crf, max_iter=200, C=100,
+                                       verbose=0, check_constraints=True,
+                                       positive_constraint=[2, 3, 4])
+        submodular_clf.fit(X, Y)
+        Y_pred = submodular_clf.predict(X)
+        # submodular crf can not do better than unaries
+        for i, x in enumerate(X):
+            y_pred_unaries = crf.inference(x, np.array([1, 1, 0, 0, 0]))
+            assert_array_equal(y_pred_unaries, Y_pred[i])
 
 
 def test_binary_ssvm_attractive_potentials():
@@ -74,3 +76,4 @@ def test_binary_ssvm_attractive_potentials():
     submodular_clf.fit(X, Y)
     Y_pred = submodular_clf.predict(X)
     assert_array_equal(Y, Y_pred)
+    assert_true(submodular_clf.w[3] < 0)  # don't ask me about signs
