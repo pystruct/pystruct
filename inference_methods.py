58a7256dccd9444a9f82c087435e40f47e96b893
Andy <amueller@ais.uni-bonn.de>
2012-12-20 18:46:03 +0100

    arbitrary graphs for ad3, again a little more precision for qpbo

--- inference_methods.py
+++ inference_methods.py
@@ -27,9 +27,9 @@ def _make_grid_edges(x, neighborhood=4, return_lists=False):
 
 
 def _inference_qpbo(x, unary_params, pairwise_params, edges):
-    unaries = (-100 * unary_params * x).astype(np.int32)
+    unaries = (-1000 * unary_params * x).astype(np.int32)
     unaries = unaries.reshape(-1, x.shape[-1])
-    pairwise = (-100 * pairwise_params).astype(np.int32)
+    pairwise = (-1000 * pairwise_params).astype(np.int32)
     edges = edges.astype(np.int32)
     y = alpha_expansion_graph(edges, unaries, pairwise, random_seed=1)
     return y.reshape(x.shape[:2])
@@ -99,8 +99,20 @@ def _inference_lp(x, unary_params, pairwise_params, edges,
 
 def _inference_ad3(x, unary_params, pairwise_params, edges,
                    relaxed=False, verbose=0):
-    raise NotImplementedError("AD3 doesn't work on graphs yet!")
-    res = AD3.simple_grid(unary_params * x, pairwise_params, verbose=verbose)
+    n_states = x.shape[-1]
+    unaries = unary_params * x.reshape(-1, n_states)
+    if pairwise_params.shape == (n_states, n_states):
+        # only one matrix given
+        edge_weights = np.repeat(pairwise_params[np.newaxis, :, :],
+                                 edges.shape[0], axis=0)
+    else:
+        if pairwise_params.shape != (edges.shape[0], n_states, n_states):
+            raise ValueError("Expected pairwise_params either to "
+                             "be of shape n_states x n_states "
+                             "or n_edges x n_states x n_states, but"
+                             " got shape %s" % repr(pairwise_params.shape))
+        edge_weights = pairwise_params
+    res = AD3.general_graph(unaries, edges, edge_weights, verbose=verbose)
     unary_marginals, pairwise_marginals, energy = res
     n_fractional = np.sum(unary_marginals.max(axis=-1) < .99)
     if n_fractional:
