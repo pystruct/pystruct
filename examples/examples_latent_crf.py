23848b3313039bf5e7bdf4e00c2c8e39c86dd1fc
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-01-11 18:33:52 +0100

    ENH simplify example for latent crf

--- examples/examples_latent_crf.py
+++ examples/examples_latent_crf.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 from sklearn.cross_validation import train_test_split
 
-from pystruct.problems import LatentDirectionalGridCRF
+from pystruct.problems import LatentGridCRF
 from pystruct.learners import LatentSSVM
 
 import pystruct.toy_datasets as toy
@@ -13,94 +13,55 @@ tracer = Tracer()
 
 
 def main():
-    #X, Y = toy.generate_crosses_latent(n_samples=25, noise=10)
-    #X, Y = toy.generate_crosses(n_samples=20, noise=10)
-    #X, Y = toy.generate_crosses(n_samples=2, noise=5, n_crosses=1,
-                                #total_size=8)
-    #X, Y = toy.generate_easy(n_samples=10, noise=5, box_size=2, total_size=6)
-    #X, Y = toy.generate_bars(n_samples=20, noise=8, bars_size=3, total_size=6)
-    X, Y = toy.generate_square_with_hole(n_samples=100, noise=5, total_size=8)
+    X, Y = toy.generate_crosses(n_samples=20, noise=5, n_crosses=1,
+                                total_size=8)
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.5)
-    #X, Y = toy.generate_xs(n_samples=25, noise=5)
     n_labels = len(np.unique(Y_train))
-    #crf = latent_crf.LatentGridCRF(n_labels=n_labels, n_states_per_label=4,
-                                   #inference_method='lp')
-    crf = LatentDirectionalGridCRF(n_labels=n_labels,
-                                              n_states_per_label=3,
-                                              inference_method='lp')
-    clf = LatentSSVM(problem=crf, max_iter=50, C=10., verbose=2,
-                     check_constraints=True, n_jobs=12, break_on_bad=True,
+    crf = LatentGridCRF(n_labels=n_labels, n_states_per_label=2,
+                        inference_method='lp')
+    clf = LatentSSVM(problem=crf, max_iter=50, C=1000., verbose=2,
+                     check_constraints=True, n_jobs=-1, break_on_bad=True,
                      plot=True)
     clf.fit(X_train, Y_train)
-    Y_pred_test = clf.predict(X_test)
-    Y_pred_train = clf.predict(X_train)
 
     i = 0
     loss = 0
-    for x, y, h_init, y_pred in zip(X_train, Y_train, clf.H_init_,
-                                    Y_pred_train):
-        loss += np.sum(y != y_pred / crf.n_states_per_label)
-        if i > 100:
-            continue
-        fig, ax = plt.subplots(3, 2)
-        ax[0, 0].matshow(y * crf.n_states_per_label,
-                         vmin=0, vmax=crf.n_states - 1)
-        ax[0, 0].set_title("ground truth")
-        #w_unaries_only = np.zeros_like(clf.w)
-        #w_unaries_only[
-        #unary_pred = crf.inference(x, w_unaries_only)
-        #ax[0, 1].matshow(unary_pred, vmin=0, vmax=crf.n_states - 1)
-        ax[0, 1].set_title("unaries only")
-        ax[1, 0].matshow(h_init, vmin=0, vmax=crf.n_states - 1)
-        ax[1, 0].set_title("latent initial")
-        ax[1, 1].matshow(crf.latent(x, y, clf.w),
-                         vmin=0, vmax=crf.n_states - 1)
-        ax[1, 1].set_title("latent final")
-        ax[2, 0].matshow(y_pred, vmin=0, vmax=crf.n_states - 1)
-        ax[2, 0].set_title("prediction")
-        ax[2, 1].matshow((y_pred // crf.n_states_per_label)
-                         * crf.n_states_per_label,
-                         vmin=0, vmax=crf.n_states - 1)
-        ax[2, 1].set_title("prediction")
-        for a in ax.ravel():
-            a.set_xticks(())
-            a.set_yticks(())
-        fig.savefig("data_train_%03d.png" % i, bbox_inches="tight")
-        i += 1
-    print("loss training set: %f" % loss)
-
-    loss = 0
-    i = 0
-    for x, y, y_pred in zip(X_test, Y_test, Y_pred_test):
-        loss += np.sum(y != y_pred / crf.n_states_per_label)
-        if i > 100:
-            continue
-        fig, ax = plt.subplots(3, 2)
-        ax[0, 0].matshow(y * crf.n_states_per_label,
-                         vmin=0, vmax=crf.n_states - 1)
-        ax[0, 0].set_title("ground truth")
-        w_unaries_only = np.zeros_like(clf.w)
-        size_unaries = len(crf.get_unary_weights(clf.w))
-        w_unaries_only[:size_unaries] = 1
-        unary_pred = crf.inference(x, w_unaries_only)
-        ax[0, 1].matshow(unary_pred, vmin=0, vmax=crf.n_states - 1)
-        ax[0, 1].set_title("unaries only")
-        ax[1, 0].set_visible(False)
-        ax[1, 1].matshow(crf.latent(x, y, clf.w),
-                         vmin=0, vmax=crf.n_states - 1)
-        ax[1, 1].set_title("latent final")
-        ax[2, 0].matshow(y_pred, vmin=0, vmax=crf.n_states - 1)
-        ax[2, 0].set_title("prediction")
-        ax[2, 1].matshow((y_pred // crf.n_states_per_label)
-                         * crf.n_states_per_label,
-                         vmin=0, vmax=crf.n_states - 1)
-        ax[2, 1].set_title("prediction")
-        for a in ax.ravel():
-            a.set_xticks(())
-            a.set_yticks(())
-        fig.savefig("data_test_%03d.png" % i, bbox_inches="tight")
-        i += 1
-    print("loss test set: %f" % loss)
+    for X_, Y_, H, name in [[X_train, Y_train, clf.H_init_, "train"],
+                            [X_test, Y_test, [None] * len(X_test), "test"]]:
+        Y_pred = clf.predict(X_)
+        for x, y, h_init, y_pred in zip(X_, Y_, H, Y_pred):
+            loss += np.sum(y != y_pred / crf.n_states_per_label)
+            fig, ax = plt.subplots(3, 2)
+            ax[0, 0].matshow(y * crf.n_states_per_label,
+                             vmin=0, vmax=crf.n_states - 1)
+            ax[0, 0].set_title("ground truth")
+            unary_params = np.repeat(np.eye(2), 2, axis=1)
+            pairwise_params = np.zeros(10)
+            w_unaries_only = np.hstack([unary_params.ravel(),
+                                        pairwise_params.ravel()])
+            unary_pred = crf.inference(x, w_unaries_only)
+            ax[0, 1].matshow(unary_pred, vmin=0, vmax=crf.n_states - 1)
+            ax[0, 1].set_title("unaries only")
+            if h_init is None:
+                ax[1, 0].set_visible(False)
+            else:
+                ax[1, 0].matshow(h_init, vmin=0, vmax=crf.n_states - 1)
+                ax[1, 0].set_title("latent initial")
+            ax[1, 1].matshow(crf.latent(x, y, clf.w),
+                             vmin=0, vmax=crf.n_states - 1)
+            ax[1, 1].set_title("latent final")
+            ax[2, 0].matshow(y_pred, vmin=0, vmax=crf.n_states - 1)
+            ax[2, 0].set_title("prediction")
+            ax[2, 1].matshow((y_pred // crf.n_states_per_label)
+                             * crf.n_states_per_label,
+                             vmin=0, vmax=crf.n_states - 1)
+            ax[2, 1].set_title("prediction")
+            for a in ax.ravel():
+                a.set_xticks(())
+                a.set_yticks(())
+            fig.savefig("data_%s_%03d.png" % (name, i), bbox_inches="tight")
+            i += 1
+        print("loss %s set: %f" % (name, loss))
     print(clf.w)
 
 if __name__ == "__main__":
