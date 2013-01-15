9a3c6cd80d9f952abada16ff56b431fc11a222fc
Andy <amueller@ais.uni-bonn.de>
2013-01-15 19:36:36 +0100

    MOVE latent_crf to latent_grid_crf

--- problems/__init__.py
+++ problems/__init__.py
@@ -2,7 +2,7 @@ from .base import StructuredProblem
 from .crf import CRF
 from .grid_crf import GridCRF, DirectionalGridCRF
 from .graph_crf import GraphCRF
-from .latent_crf import LatentGridCRF, LatentDirectionalGridCRF
+from .latent_grid_crf import LatentGridCRF, LatentDirectionalGridCRF
 from .unstructured_svm import BinarySVMProblem, CrammerSingerSVMProblem
 
 __all__ = ["StructuredProblem", "CRF", "GridCRF", "GraphCRF",
