5b63c54d861b437cda988bcda1d95f61fb7b6c1f
Andy <amueller@ais.uni-bonn.de>
2013-01-16 15:05:45 +0100

    Adding LatentGraphCRF to problems __init__

--- problems/__init__.py
+++ problems/__init__.py
@@ -3,9 +3,10 @@ from .crf import CRF
 from .grid_crf import GridCRF, DirectionalGridCRF
 from .graph_crf import GraphCRF
 from .latent_grid_crf import LatentGridCRF, LatentDirectionalGridCRF
+from .latent_graph_crf import LatentGraphCRF
 from .unstructured_svm import BinarySVMProblem, CrammerSingerSVMProblem
 
 __all__ = ["StructuredProblem", "CRF", "GridCRF", "GraphCRF",
            "DirectionalGridCRF", "BinarySVMProblem",
            "LatentGridCRF", "LatentDirectionalGridCRF",
-           "CrammerSingerSVMProblem"]
+           "CrammerSingerSVMProblem", "LatentGraphCRF"]
