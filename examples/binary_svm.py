ec5502fe95dc2ef702d4af29ddda0c44f8485777
Andy <amueller@ais.uni-bonn.de>
2013-01-23 16:35:34 +0100

    EXAMPLE: compare svm solvers on digits

--- examples/binary_svm.py
+++ examples/binary_svm.py
@@ -8,7 +8,8 @@ from sklearn.cross_validation import train_test_split
 from sklearn.svm import SVC
 
 from pystruct.problems import BinarySVMProblem
-from pystruct.learners import StructuredSVM
+from pystruct.learners import (StructuredSVM, OneSlackSSVM,
+                               SubgradientStructuredSVM)
 
 # do a binary digit classification
 digits = load_digits()
@@ -23,17 +24,41 @@ X /= X.max()
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 pbl = BinarySVMProblem(n_features=X_train.shape[1] + 1)  # add one for bias
-svm = StructuredSVM(pbl, verbose=2, check_constraints=False, C=20)
+n_slack_svm = StructuredSVM(pbl, verbose=0, check_constraints=False, C=20,
+                            batch_size=-1)
+one_slack_svm = OneSlackSSVM(pbl, verbose=0, check_constraints=False, C=500,
+                             max_iter=1000, tol=1e-10)
+subgradient_svm = SubgradientStructuredSVM(pbl, batch=False, C=20,
+                                           learning_rate=0.0001, max_iter=50)
 
-start = time()
 # we add a constant 1 feature for the bias
 X_train_bias = np.hstack([X_train, np.ones((X_train.shape[0], 1))])
-svm.fit(X_train_bias, y_train)
-time_svm = time() - start
 X_test_bias = np.hstack([X_test, np.ones((X_test.shape[0], 1))])
-y_pred = np.hstack(svm.predict(X_test_bias))
-print("Score with pystruct toy svm: %f (took %f seconds)"
-      % (np.mean(y_pred == y_test), time_svm))
+
+# n-slack cutting plane ssvm
+start = time()
+n_slack_svm.fit(X_train_bias, y_train)
+time_n_slack_svm = time() - start
+y_pred = np.hstack(n_slack_svm.predict(X_test_bias))
+print("Score with pystruct n-slack ssvm: %f (took %f seconds)"
+      % (np.mean(y_pred == y_test), time_n_slack_svm))
+
+## 1-slack cutting plane ssvm
+start = time()
+one_slack_svm.fit(X_train_bias, y_train)
+time_one_slack_svm = time() - start
+y_pred = np.hstack(one_slack_svm.predict(X_test_bias))
+print("Score with pystruct 1-slack ssvm: %f (took %f seconds)"
+      % (np.mean(y_pred == y_test), time_one_slack_svm))
+
+# online subgradient ssvm
+start = time()
+subgradient_svm.fit(X_train_bias, y_train)
+time_subgradient_svm = time() - start
+y_pred = np.hstack(subgradient_svm.predict(X_test_bias))
+
+print("Score with pystruct subgradient ssvm: %f (took %f seconds)"
+      % (np.mean(y_pred == y_test), time_subgradient_svm))
 
 # because of the way I construct psi, we use half the C
 libsvm = SVC(kernel='linear', C=10)
