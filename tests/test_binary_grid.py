ba7378550a389fd6f6c5e40e260df6bf68a09655
Andy <amueller@ais.uni-bonn.de>
2013-01-25 16:13:40 +0100

    remove unnecessary test: all subgradient learning is now online.

--- tests/test_binary_grid.py
+++ tests/test_binary_grid.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.testing import assert_array_equal
-from nose.tools import assert_true, assert_greater
+from nose.tools import assert_true
 from pystruct.problems import GridCRF
 from pystruct.learners import StructuredSVM, SubgradientStructuredSVM
 import pystruct.toy_datasets as toy
@@ -24,22 +24,10 @@ def test_binary_blocks_subgradient():
     X, Y = toy.generate_blocks(n_samples=10)
     crf = GridCRF()
     clf = SubgradientStructuredSVM(problem=crf, max_iter=200, C=100,
-                                   verbose=10, momentum=.0, learning_rate=0.1)
+                                   verbose=10, learning_rate=0.1)
     clf.fit(X, Y)
     Y_pred = clf.predict(X)
     assert_array_equal(Y, Y_pred)
-
-
-def test_binary_blocks_subgradient_online():
-    #testing subgradient ssvm on easy binary dataset with online learning
-    X, Y = toy.generate_blocks(n_samples=10)
-    crf = GridCRF()
-    clf = SubgradientStructuredSVM(problem=crf, max_iter=200, C=10000,
-                                   verbose=10, momentum=.9, learning_rate=0.1,
-                                   batch=False)
-    clf.fit(X, Y)
-    Y_pred = clf.predict(X)
-    assert_greater(3, np.sum(Y - Y_pred))
 
 
 def test_binary_checker_subgradient():
