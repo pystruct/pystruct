e671d998290c22d6c20cf077635664f03e7e2922
Andy <amueller@ais.uni-bonn.de>
2013-01-28 17:16:53 +0100

    fixed plotting in latent_crf example

--- examples/latent_crf.py
+++ examples/latent_crf.py
@@ -28,15 +28,10 @@ def main():
         for x, y, h_init, y_pred in zip(X_, Y_, H, Y_pred):
             loss += np.sum(y != y_pred / crf.n_states_per_label)
             fig, ax = plt.subplots(3, 2)
-            ax[0, 0].matshow(y * crf.n_states_per_label,
-                             vmin=0, vmax=crf.n_states - 1)
+            ax[0, 0].matshow(y, vmin=0, vmax=crf.n_labels - 1)
             ax[0, 0].set_title("ground truth")
-            unary_params = np.repeat(np.eye(2), 2, axis=1)
-            pairwise_params = np.zeros(10)
-            w_unaries_only = np.hstack([unary_params.ravel(),
-                                        pairwise_params.ravel()])
-            unary_pred = crf.inference(x, w_unaries_only)
-            ax[0, 1].matshow(unary_pred, vmin=0, vmax=crf.n_states - 1)
+            ax[0, 1].matshow(np.argmax(x, axis=-1),
+                             vmin=0, vmax=crf.n_labels - 1)
             ax[0, 1].set_title("unaries only")
             if h_init is None:
                 ax[1, 0].set_visible(False)
@@ -48,9 +43,8 @@ def main():
             ax[1, 1].set_title("latent final")
             ax[2, 0].matshow(y_pred, vmin=0, vmax=crf.n_states - 1)
             ax[2, 0].set_title("prediction")
-            ax[2, 1].matshow((y_pred // crf.n_states_per_label)
-                             * crf.n_states_per_label,
-                             vmin=0, vmax=crf.n_states - 1)
+            ax[2, 1].matshow((y_pred // crf.n_states_per_label),
+                             vmin=0, vmax=crf.n_labels - 1)
             ax[2, 1].set_title("prediction")
             for a in ax.ravel():
                 a.set_xticks(())
