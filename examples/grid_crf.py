bddd6f21361e4364432313b45a0ba5e3b3368dcb
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-02-28 16:00:21 +0100

    make grid_crf example way faster with less cores

--- examples/grid_crf.py
+++ examples/grid_crf.py
@@ -17,8 +17,8 @@ def main():
     crf = DirectionalGridCRF(n_states=n_labels, inference_method="lp",
                              neighborhood=4)
     clf = ssvm.OneSlackSSVM(problem=crf, max_iter=1000, C=100, verbose=2,
-                            check_constraints=True, n_jobs=12,
-                            inference_cache=1000, inactive_window=0)
+                            check_constraints=True, n_jobs=-1,
+                            inference_cache=100, inactive_window=50, tol=.1)
     #clf = ssvm.StructuredSVM(problem=crf, max_iter=100, C=100, verbose=3,
                              #check_constraints=True, n_jobs=12)
     #clf = StructuredPerceptron(problem=crf, max_iter=1000, verbose=10)
