b290dad4728d1fbc26b67a3b0fc550ed666d4f99
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-07-08 09:18:07 +0200

    slight refactoring (make n_vertices optional in is_tree, introduce edges_to_graph, start on message passing

--- pystruct/inference/maxprod.py
+++ pystruct/inference/maxprod.py
@@ -1,26 +1,63 @@
 import numpy as np
 from scipy import sparse
+from scipy.sparse import csgraph
 
 
-def is_tree(n_vertices, edges):
-    # tests if graph given by edges is a tree
-    # checks for each connected component if the number of edges is smaller
-    # than number of vertices.
-
-    if len(edges) > n_vertices - 1:
-        return False
+def edges_to_graph(edges, n_vertices=None):
+    if n_vertices is None:
+        n_vertices = np.max(edges) + 1
     graph = sparse.coo_matrix((np.ones(len(edges)), edges.T),
                               shape=(n_vertices, n_vertices)).tocsr()
+    return graph
+
+
+def is_tree(edges, n_vertices=None):
+    """Check if edges specify a tree.
+
+    Parameters
+    ----------
+    edges : nd-array of int
+        Edges of a graph. Shape (n_edges, 2).
+    n_vertices : int or None
+        Number of vertices in the graph. If None, it is inferred from the
+        edges.
+    """
+    if n_vertices is None:
+        n_vertices = np.max(edges) + 1
+    if len(edges) > n_vertices - 1:
+        return False
+    graph = edges_to_graph(edges, n_vertices)
     n_components, component_indicators = \
-        sparse.csgraph.connected_components(graph, directed=False)
+        csgraph.connected_components(graph, directed=False)
     if len(edges) > n_vertices - n_components:
         return False
     return True
 
 
-def max_product(unary_potentials, pairwise_potentials, edges):
-    if is_tree(n_vertices=len(unary_potentials), edges=edges):
+def inference_max_product(unary_potentials, pairwise_potentials, edges):
+    """Max-product inference.
+
+    In case the edges specify a tree, dynamic programming is used
+    producing a result in only a single pass.
+    """
+    if is_tree(edges=edges, n_vertices=len(unary_potentials)):
         y = tree_max_product(unary_potentials, pairwise_potentials, edges)
     else:
         y = iterative_max_product(unary_potentials, pairwise_potentials)
     return y
+
+
+def tree_max_product(unary_potentials, pairwise_potentials, edges):
+    graph = edges_to_graph(edges, len(unary_potentials))
+    nodes, predecessors = csgraph.depth_first_order(graph, 0, directed=False)
+    for i, node in enumerate(nodes):
+        for neighbor in get_neighbors(edges, node):
+            if neighbor == predecessors[i]:
+                continue
+
+    from IPython.core.debugger import Tracer
+    Tracer()()
+
+
+def iterative_max_product():
+    pass
--- tests/test_inference/test_maxprod.py
+++ tests/test_inference/test_maxprod.py
@@ -1,33 +1,47 @@
 import numpy as np
 from nose.tools import assert_true, assert_false
+from numpy.testing import assert_array_equal
 from scipy import sparse
 
-from sklearn.utils.mst import minimum_spanning_tree
-
-from pystruct.inference.maxprod import is_tree
+from pystruct.inference.maxprod import is_tree, inference_max_product
+from pystruct.inference import inference_ad3
 
 
 def test_is_tree():
     # generate chain
     chain = np.c_[np.arange(1, 10), np.arange(9)]
-    assert_true(is_tree(len(chain) + 1, chain))
+    assert_true(is_tree(chain, len(chain) + 1))
+    assert_true(is_tree(chain))
     # generate circle
     circle = np.vstack([chain, [9, 0]])
-    assert_false(is_tree(len(chain) + 1, circle))
+    assert_false(is_tree(circle))
+    assert_false(is_tree(circle, len(chain) + 1))
 
     # union of two disjoint chains
     two_chains = np.vstack([chain, chain + 10])
-    assert_true(is_tree(20, two_chains))
+    assert_true(is_tree(two_chains, 20))
 
     # union of chain and circle
     disco_graph = np.vstack([chain, circle + 10])
-    assert_false(is_tree(20, disco_graph))
+    assert_false(is_tree(disco_graph))
 
     # generate random fully connected graph
     graph = np.random.uniform(size=(10, 10))
     edges = np.c_[graph.nonzero()]
-    assert_false(is_tree(10, edges))
+    assert_false(is_tree(edges))
 
-    tree = minimum_spanning_tree(sparse.csr_matrix(graph))
+    tree = sparse.csgraph.minimum_spanning_tree(sparse.csr_matrix(graph))
     tree_edges = np.c_[tree.nonzero()]
-    assert_true(is_tree(10, tree_edges))
+    assert_true(is_tree(tree_edges, 10))
+    assert_true(is_tree(tree_edges))
+
+
+def test_tree_max_product():
+    chain = np.c_[np.arange(1, 10), np.arange(9)]
+    unary_potentials = np.random.normal(size=(10, 3))
+    pairwise_potentials = np.random.normal(size=(3, 3))
+    result_ad3 = inference_ad3(unary_potentials, pairwise_potentials, chain,
+                               branch_and_bound=True)
+    result_mp = inference_max_product(unary_potentials, pairwise_potentials,
+                                      chain)
+    assert_array_equal(result_ad3, result_mp)
