bd1db15ea78a91b9b4262056b23b4d47780d51c1
Andy <amueller@ais.uni-bonn.de>
2013-07-16 16:50:57 +0200

    enh: augment string parameter for algorithms by optional dict of kwargs

--- tests/test_inference/test_exact_inference.py
+++ tests/test_inference/test_exact_inference.py
@@ -1,41 +1,32 @@
 import numpy as np
 from numpy.testing import assert_array_equal
 
-from pystruct.inference import inference_lp
+from pystruct.inference import get_installed, inference_dispatch
 
 
 def test_chain():
     # test LP, AD3, AD3-BB and JT on a chain.
     # they should all be exact
     rnd = np.random.RandomState(0)
+    algorithms = get_installed([('ad3', {'branch_and_bound':False}),
+                                ('ad3', {'branch_and_bound':True}),
+                                ('dai', {'alg':'jt'})])
     for i in xrange(10):
         forward = np.c_[np.arange(9), np.arange(1, 10)]
         backward = np.c_[np.arange(1, 10), np.arange(9)]
         unary_potentials = rnd.normal(size=(10, 3))
         pairwise_potentials = rnd.normal(size=(3, 3))
         # test that reversing edges is same as transposing pairwise potentials
-        y_forward = inference_lp(unary_potentials, pairwise_potentials,
-                                 forward)
-        y_backward = inference_lp(unary_potentials, pairwise_potentials.T,
-                                  backward)
+        y_forward = inference_dispatch(unary_potentials, pairwise_potentials,
+                                       forward, 'lp')
+        y_backward = inference_dispatch(unary_potentials,
+                                        pairwise_potentials.T, backward, 'lp')
         assert_array_equal(y_forward, y_backward)
         for chain in [forward, backward]:
-            y_lp = inference_lp(unary_potentials, pairwise_potentials, chain)
-            try:
-                from pystruct.inference import inference_dai
-                y_dai = inference_dai(unary_potentials, pairwise_potentials,
-                                      chain, alg='jt')
-                assert_array_equal(y_dai, y_lp)
-            except:
-                pass
-            try:
-                from pystruct.inference import inference_ad3
-
-                y_ad3 = inference_ad3(unary_potentials, pairwise_potentials,
-                                      chain)
-                y_ad3bb = inference_ad3(unary_potentials, pairwise_potentials,
-                                        chain, branch_and_bound=True)
-                assert_array_equal(y_ad3, y_lp)
-                assert_array_equal(y_ad3bb, y_lp)
-            except:
-                pass
+            y_lp = inference_dispatch(unary_potentials, pairwise_potentials,
+                                      chain, 'lp')
+            for alg in algorithms:
+                print(alg)
+                y = inference_dispatch(unary_potentials, pairwise_potentials,
+                                       chain, alg)
+                assert_array_equal(y, y_lp)
