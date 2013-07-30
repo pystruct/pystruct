70678db0121adec73aed30e1681df66dd0b525ce
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-07-30 14:22:53 +0200

    really rename pbl to model in example :-/

--- examples/multi_class_svm.py
+++ examples/multi_class_svm.py
@@ -32,12 +32,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y)
 X_train_bias = np.hstack([X_train, np.ones((X_train.shape[0], 1))])
 X_test_bias = np.hstack([X_test, np.ones((X_test.shape[0], 1))])
 
-pbl = CrammerSingerSVMModel(n_features=X_train_bias.shape[1], n_classes=10)
-n_slack_svm = NSlackSSVM(pbl, verbose=0, check_constraints=False, C=20,
+model = CrammerSingerSVMModel(n_features=X_train_bias.shape[1], n_classes=10)
+n_slack_svm = NSlackSSVM(model, verbose=0, check_constraints=False, C=20,
                          batch_size=-1, tol=1e-2)
-one_slack_svm = OneSlackSSVM(pbl, verbose=50, check_constraints=False, C=.20,
+one_slack_svm = OneSlackSSVM(model, verbose=50, check_constraints=False, C=.20,
                              max_iter=10000, tol=.001, show_loss_every=10)
-subgradient_svm = SubgradientSSVM(pbl, C=20, learning_rate=0.000001,
+subgradient_svm = SubgradientSSVM(model, C=20, learning_rate=0.000001,
                                   max_iter=1000, verbose=0)
 
 # n-slack cutting plane ssvm
