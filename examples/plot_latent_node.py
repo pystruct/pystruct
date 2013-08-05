ad4311cda25011b78aa7954c1c6910f5965c0116
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-08-05 20:55:17 +0200

    EXAMPLE Cosmetic touches, faster learning

--- examples/plot_latent_node.py
+++ examples/plot_latent_node.py
@@ -9,10 +9,9 @@ import numpy as np
 import itertools
 
 from pystruct.models import GraphCRF, LatentNodeCRF
-from pystruct.learners import NSlackSSVM
-from pystruct.learners import LatentSubgradientSSVM
+from pystruct.learners import NSlackSSVM, OneSlackSSVM, LatentSSVM
 import pystruct.toy_datasets as toy
-from pystruct.utils import make_grid_edges
+from pystruct.utils import make_grid_edges, plot_grid
 import matplotlib.pyplot as plt
 
 
@@ -21,25 +20,22 @@ def plot_boxes(boxes, size=4, title=""):
     if boxes[0].size == size * size:
         fig, ax = plt.subplots(1, len(boxes), figsize=(8, 0.7))
         for a, x in zip(ax, boxes):
-            a.matshow(x[:size * size].reshape(size, size), cmap=cmap)
-            a.set_xticks(())
-            a.set_yticks(())
+            plot_grid(x[:size * size].reshape(size, size), cmap=cmap, axes=a,
+                      border_color="green")
     else:
         # have hidden states
         fig, ax = plt.subplots(2, len(boxes), figsize=(8, 1))
         for a, x in zip(ax[0], boxes):
-            a.matshow(x[size * size:].reshape(size / 2, size / 2), cmap=cmap)
-            a.set_xticks(())
-            a.set_yticks(())
+            plot_grid(x[size * size:].reshape(size / 2, size / 2), cmap=cmap,
+                      axes=a, border_color="green")
         for a, x in zip(ax[1], boxes):
-            a.matshow(x[:size * size].reshape(size, size), cmap=cmap)
-            a.set_xticks(())
-            a.set_yticks(())
+            plot_grid(x[:size * size].reshape(size, size), cmap=cmap, axes=a,
+                      border_color="green")
     fig.subplots_adjust(.01, .03, .98, .75, .2, .05)
     fig.suptitle(title)
 
 
-# learn the "easy" 3x2 boxes dataset.
+# learn the "easy" 2x2 boxes dataset.
 # a 2x2 box is placed randomly in a 4x4 grid
 # we add a latent variable for each 2x2 patch
 # that should make the model fairly simple
@@ -52,7 +48,7 @@ Y_flat = [y.ravel() for y in Y]
 
 
 # first, use standard graph CRF. Can't do much, high loss.
-crf = GraphCRF(n_states=2, n_features=1, inference_method='lp')
+crf = GraphCRF()
 svm = NSlackSSVM(model=crf, max_iter=200, C=1, verbose=0,
                  check_constraints=True, break_on_bad=False, n_jobs=1)
 
@@ -63,15 +59,15 @@ G = [make_grid_edges(x) for x in X]
 asdf = zip(X_flat, G)
 svm.fit(asdf, Y_flat)
 plot_boxes(svm.predict(asdf), title="Non-latent SSVM predictions")
-print("Training score multiclass svm CRF: %f" % svm.score(asdf, Y_flat))
+print("Training score binary SVM: %f" % svm.score(asdf, Y_flat))
 
 # using one latent variable for each 2x2 rectangle
-latent_crf = LatentNodeCRF(n_labels=2, n_features=1, inference_method='lp',
-                           n_hidden_states=2)
+latent_crf = LatentNodeCRF(n_labels=2, n_features=1, n_hidden_states=2,
+                           inference_method='lp')
 
-latent_svm = LatentSubgradientSSVM(model=latent_crf, max_iter=200, C=100,
-                                   verbose=0, n_jobs=1, show_loss_every=10,
-                                   learning_rate=0.01, momentum=0)
+ssvm = OneSlackSSVM(model=latent_crf, max_iter=200, C=100, verbose=1,
+                    n_jobs=-1, show_loss_every=10, inference_cache=50)
+latent_svm = LatentSSVM(ssvm)
 
 # make edges for hidden states:
 edges = []
@@ -86,7 +82,8 @@ G = [np.vstack([make_grid_edges(x), edges]) for x in X]
 # Random initialization
 H_init = [np.hstack([y.ravel(), np.random.randint(2, 4, size=2 * 2)])
           for y in Y]
-plot_boxes(H_init, title="Initial random hidden states")
+plot_boxes(H_init, title="Top: Random initial hidden states. Bottom: Ground"
+           "truth labeling.")
 
 X_ = zip(X_flat, G, [2 * 2 for x in X_flat])
 
@@ -94,5 +91,5 @@ latent_svm.fit(X_, Y_flat, H_init)
 
 print("Training score with latent nodes: %f " % latent_svm.score(X_, Y_flat))
 H = latent_svm.predict_latent(X_)
-plot_boxes(H, title="Hidden states after training")
+plot_boxes(H, title="Top: Hidden states after training. Bottom: Prediction.")
 plt.show()
--- pystruct/utils.py
+++ pystruct/utils.py
@@ -352,3 +352,43 @@ def train_test_split(*arrays, **options):
     return splitted
 
 train_test_split.__test__ = False  # to avoid a pb with nosetests
+
+
+def plot_grid(x, cmap=None, border_color=None, axes=None, linewidth=1):
+    """Plot a grid labeling including grid boundaries.
+
+    Parameters
+    ==========
+    x : array-like, 2d
+        Input array to plot.
+
+    cmap : matplotlib colormap
+        Colormap to be used for array.
+
+    border_color : matplotlib color
+        Color to be used to plot borders between grid-cells.
+
+    axes : matplotlib axes object
+        We will plot into this.
+
+    linewidth : int
+        Linewidth for the grid-cell borders.
+
+    Returns
+    =======
+    axes : matplotlib axes object
+        The axes object that contains the plot.
+    """
+    import matplotlib.pyplot as plt
+    if axes is not None:
+        axes.matshow(x, cmap=cmap)
+    else:
+        axes = plt.matshow(x, cmap=cmap).get_axes()
+    axes.set_xticks(np.arange(1, x.shape[1]) - .5)
+    axes.set_yticks(np.arange(1, x.shape[0]) - .5)
+    if border_color is None:
+        border_color = 'black'
+    axes.grid(linestyle="-", linewidth=linewidth, color=border_color)
+    axes.set_xticklabels(())
+    axes.set_yticklabels(())
+    return axes
