7e1332c18d1cfe5b1d4d29b533c8f309339d4ae4
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-08-06 15:04:24 +0200

    cleaning up utils module

---
+++ pystruct/utils/__init__.py
@@ -0,0 +1,14 @@
+from backports import train_test_split
+from inference import (unwrap_pairwise, make_grid_edges, compute_energy,
+                       find_constraint, find_constraint_latent, inference,
+                       loss_augmented_inference, objective_primal,
+                       exhaustive_loss_augmented_inference,
+                       exhaustive_inference)
+from logging import SaveLogger
+from plotting import plot_grid
+
+__all__ = ["train_test_split", "unwrap_pairwise",
+           "make_grid_edges", "compute_energy", "find_constraint",
+           "find_constraint_latent", "inference", "loss_augmented_inference",
+           "objective_primal", "exhaustive_loss_augmented_inference",
+           "exhaustive_inference", "SaveLogger", "plot_grid"]
--- src/utilities.pyx
+++
@@ -1,9 +0,0 @@
-# cython: boundscheck=False
-# cython: wraparound=False
-
-def crammer_singer_psi(double[:,:] X, long[:] Y, double[:, :] out):
-    cdef int y, i
-    for i in xrange(X.shape[0]):
-        y = Y[i]
-        for j in xrange(X.shape[1]):
-            out[y, j] += X[i, j]
