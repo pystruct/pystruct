fcec7aa54630a41d0fa0c282bde6abb54fb4ed99
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-08-07 20:27:48 +0200

    make datasets a subfolder

---
+++ pystruct/datasets/__init__.py
@@ -0,0 +1,6 @@
+from .synthetic_grids import (generate_blocks, generate_blocks_multinomial,
+                              generate_bars, generate_crosses_explicit, binary,
+                              multinomial)
+
+__all__ = ['generate_blocks', 'generate_blocks_multinomial', 'generate_bars',
+           'generate_crosses_explicit', 'binary', 'multinomial']
