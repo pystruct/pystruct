83f6addabaed65bfb71c8dfe15a3ec7f6ac779c4
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-08-15 16:01:52 +0200

    EXAMPLE Add BCFW to examples

--- examples/multi_class_svm.py
+++ examples/multi_class_svm.py
@@ -9,22 +9,22 @@ Comparing different solvers on a standard multi-class SVM problem.
 from time import time
 import numpy as np
 
-from sklearn.datasets import fetch_mldata
-#from sklearn.datasets import load_digits
+#from sklearn.datasets import fetch_mldata
+from sklearn.datasets import load_digits
 from sklearn.cross_validation import train_test_split
 from sklearn.svm import LinearSVC
 
 from pystruct.models import MultiClassClf
 from pystruct.learners import (NSlackSSVM, OneSlackSSVM,
-                               SubgradientSSVM)
+                               SubgradientSSVM, FrankWolfeSSVM)
 
 # do a binary digit classification
-digits = fetch_mldata("USPS")
-#digits = load_digits()
+#digits = fetch_mldata("USPS")
+digits = load_digits()
 X, y = digits.data, digits.target
 #X = X / 255.
-#X = X / 16.
-y = y.astype(np.int) - 1
+X = X / 16.
+#y = y.astype(np.int) - 1
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 # we add a constant 1 feature for the bias
@@ -38,13 +38,16 @@ one_slack_svm = OneSlackSSVM(model, verbose=2, C=.10, tol=.001)
 subgradient_svm = SubgradientSSVM(model, C=0.1, learning_rate=0.000001,
                                   max_iter=1000, verbose=0)
 
+fw_bc_svm = FrankWolfeSSVM(model, C=.1, max_iter=50)
+fw_batch_svm = FrankWolfeSSVM(model, C=.1, max_iter=50, batch_mode=True)
+
 # n-slack cutting plane ssvm
-#start = time()
-#n_slack_svm.fit(X_train_bias, y_train)
-#time_n_slack_svm = time() - start
-#y_pred = np.hstack(n_slack_svm.predict(X_test_bias))
-#print("Score with pystruct n-slack ssvm: %f (took %f seconds)"
-      #% (np.mean(y_pred == y_test), time_n_slack_svm))
+start = time()
+n_slack_svm.fit(X_train_bias, y_train)
+time_n_slack_svm = time() - start
+y_pred = np.hstack(n_slack_svm.predict(X_test_bias))
+print("Score with pystruct n-slack ssvm: %f (took %f seconds)"
+      % (np.mean(y_pred == y_test), time_n_slack_svm))
 
 ## 1-slack cutting plane ssvm
 start = time()
@@ -55,23 +58,36 @@ print("Score with pystruct 1-slack ssvm: %f (took %f seconds)"
       % (np.mean(y_pred == y_test), time_one_slack_svm))
 
 #online subgradient ssvm
-#start = time()
-#subgradient_svm.fit(X_train_bias, y_train)
-#time_subgradient_svm = time() - start
-#y_pred = np.hstack(subgradient_svm.predict(X_test_bias))
+start = time()
+subgradient_svm.fit(X_train_bias, y_train)
+time_subgradient_svm = time() - start
+y_pred = np.hstack(subgradient_svm.predict(X_test_bias))
 
-#print("Score with pystruct subgradient ssvm: %f (took %f seconds)"
-      #% (np.mean(y_pred == y_test), time_subgradient_svm))
+print("Score with pystruct subgradient ssvm: %f (took %f seconds)"
+      % (np.mean(y_pred == y_test), time_subgradient_svm))
 
-# because of the way I construct psi, we use half the C
 # the standard one-vs-rest multi-class would probably be as good and faster
 # but solving a different model
-#libsvm = LinearSVC(multi_class='crammer_singer', C=.1)
-#start = time()
-#libsvm.fit(X_train, y_train)
-#time_libsvm = time() - start
-#print("Score with sklearn and libsvm: %f (took %f seconds)"
-      #% (libsvm.score(X_test, y_test), time_libsvm))
+libsvm = LinearSVC(multi_class='crammer_singer', C=.1)
+start = time()
+libsvm.fit(X_train, y_train)
+time_libsvm = time() - start
+print("Score with sklearn and libsvm: %f (took %f seconds)"
+      % (libsvm.score(X_test, y_test), time_libsvm))
 
+
+start = time()
+fw_bc_svm.fit(X_train_bias, y_train)
+y_pred = np.hstack(fw_bc_svm.predict(X_test_bias))
+time_fw_bc_svm = time() - start
+print("Score with pystruct frankwolfe block coordinate ssvm: %f (took %f seconds)" %
+      (np.mean(y_pred == y_test), time_fw_bc_svm))
+
+start = time()
+fw_batch_svm.fit(X_train_bias, y_train)
+y_pred = np.hstack(fw_batch_svm.predict(X_test_bias))
+time_fw_batch_svm = time() - start
+print("Score with pystruct frankwolfe batch ssvm: %f (took %f seconds)" %
+      (np.mean(y_pred == y_test), time_fw_batch_svm))
 #from IPython.core.debugger import Tracer
 #Tracer()()
--- examples/plot_svm_objectives.py
+++ examples/plot_svm_objectives.py
@@ -14,10 +14,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from pystruct.models import GridCRF
-from pystruct.learners import (NSlackSSVM, OneSlackSSVM, SubgradientSSVM)
+from pystruct.learners import (NSlackSSVM, OneSlackSSVM, SubgradientSSVM,
+                               FrankWolfeSSVM)
 from pystruct.datasets import generate_crosses_explicit
 
-X, Y = generate_crosses_explicit(n_samples=50, noise=10, size=6, n_crosses=1)
+X, Y = generate_crosses_explicit(n_samples=500, noise=10, size=6, n_crosses=1)
 n_labels = len(np.unique(Y))
 crf = GridCRF(n_states=n_labels, inference_method="dai")
 
@@ -27,6 +28,7 @@ one_slack_svm = OneSlackSSVM(crf, check_constraints=False,
                              max_iter=100, tol=0.001, inference_cache=50)
 subgradient_svm = SubgradientSSVM(crf, learning_rate=0.001, max_iter=20,
                                   decay_exponent=0, momentum=0)
+bcfw_svm = FrankWolfeSSVM(crf, max_iter=400, verbose=2, check_dual_every=4)
 
 #n-slack cutting plane ssvm
 n_slack_svm.fit(X, Y)
@@ -36,6 +38,9 @@ one_slack_svm.fit(X, Y)
 
 # online subgradient ssvm
 subgradient_svm.fit(X, Y)
+
+# Block coordinate Frank-Wolfe
+bcfw_svm.fit(X, Y)
 
 # don't plot objective from chached inference for 1-slack
 inference_run = ~np.array(one_slack_svm.cached_constraint_)
@@ -54,6 +59,10 @@ plt.plot(time_one,
          label="one-slack primal")
 plt.plot(subgradient_svm.timestamps_[1:], subgradient_svm.objective_curve_,
          label="subgradient")
+plt.plot(bcfw_svm.timestamps_[1:], bcfw_svm.objective_curve_,
+         label="Block-Coordinate Frank-Wolfe Dual")
+plt.plot(bcfw_svm.timestamps_[1:], bcfw_svm.primal_objective_curve_,
+         label="Block-Coordinate Frank-Wolfe Primal")
 plt.legend()
 plt.yscale('log')
 plt.xlabel("training time")
