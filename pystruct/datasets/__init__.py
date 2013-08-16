f39556f03028e3801da6579d1651411ef9b7d37d
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-08-16 16:57:18 +0200

    ENH add snakes dataset, put all datasets that are simply loaded from a pickle into one file.

--- pystruct/datasets/__init__.py
+++ pystruct/datasets/__init__.py
@@ -4,11 +4,10 @@ from .synthetic_grids import (generate_blocks, generate_blocks_multinomial,
                               generate_crosses, generate_checker,
                               generate_checker_multinomial,
                               generate_big_checker)
-from .scene import load_scene
-from .letters import load_letters
+from .dataset_loaders import load_scene, load_letters, load_snakes
 
 __all__ = ['generate_blocks', 'generate_blocks_multinomial', 'generate_bars',
            'generate_crosses_explicit', 'binary', 'multinomial', 'load_scene',
            'make_simple_2x2', 'generate_easy', 'generate_crosses',
            'generate_checker', 'generate_checker_multinomial',
-           'generate_big_checker', 'load_letters']
+           'generate_big_checker', 'load_letters', 'load_snakes']
--- pystruct/datasets/scene.py
+++
@@ -1,9 +0,0 @@
-import cPickle
-from os.path import dirname
-from os.path import join
-
-
-def load_scene():
-    module_path = dirname(__file__)
-    data_file = open(join(module_path, 'scene.pickle'))
-    return cPickle.load(data_file)
