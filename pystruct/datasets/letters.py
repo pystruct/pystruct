666ff8efad32f81381cc09e09e37231b0c5d358f
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-08-16 16:57:18 +0200

    ENH add snakes dataset, put all datasets that are simply loaded from a pickle into one file.

--- pystruct/datasets/letters.py
+++ pystruct/datasets/letters.py
@@ -4,19 +4,3 @@ from os.path import join
 
 import numpy as np
 
-
-def load_letters():
-    """Load the OCR letters dataset.
-
-    This is a chain classification task.
-    Each example consists of a word, segmented into letters.
-    The first letter of each word is ommited from the data,
-    as it was a capital letter (in contrast to all other letters).
-    """
-    module_path = dirname(__file__)
-    data_file = open(join(module_path, 'letters.pickle'))
-    data = cPickle.load(data_file)
-    # we add an easy to use image representation:
-    data['images'] = [np.hstack([l.reshape(16, 8) for l in word])
-                      for word in data['data']]
-    return data
