333fbe90b78f11d99dc17ef6a345a0493a8a17cf
Andy <amueller@ais.uni-bonn.de>
2013-08-27 10:57:27 +0200

    EXAMPLE Switch from named tuple to dict, update url for pascal example

--- examples/image_segmentation.py
+++ examples/image_segmentation.py
@@ -5,7 +5,7 @@ Semantic Image Segmentation on Pascal VOC
 This example demonstrates learning a superpixel CRF
 for semantic image segmentation.
 To run the experiment, please download the pre-processed data from:
-http://www.ais.uni-bonn.de/~amueller/data/
+http://www.ais.uni-bonn.de/deep_learning/downloads.html
 
 The data consists of superpixels, unary potentials, and the connectivity
 structure of the superpixels.
@@ -27,12 +27,12 @@ import pystruct.models as crfs
 from pystruct.utils import SaveLogger
 
 
-data_train = cPickle.load(open("data_train.pickle"))
+data_train = cPickle.load(open("data_train_dict.pickle"))
 C = 0.01
 
 n_states = 21
-print("number of samples: %s" % len(data_train.X))
-class_weights = 1. / np.bincount(np.hstack(data_train.Y))
+print("number of samples: %s" % len(data_train['X']))
+class_weights = 1. / np.bincount(np.hstack(data_train['Y']))
 class_weights *= 21. / np.sum(class_weights)
 print(class_weights)
 
@@ -48,13 +48,13 @@ ssvm = learners.NSlackSSVM(
     tol=0.0001, show_loss_every=5,
     logger=SaveLogger(experiment_name + ".pickle", save_every=100),
     inactive_threshold=1e-3, inactive_window=10, batch_size=100)
-ssvm.fit(data_train.X, data_train.Y)
+ssvm.fit(data_train['X'], data_train['Y'])
 
-data_val = cPickle.load(open("data_val.pickle"))
-y_pred = ssvm.predict(data_val.X)
+data_val = cPickle.load(open("data_val_dict.pickle"))
+y_pred = ssvm.predict(data_val['X'])
 
 # we throw away void superpixels and flatten everything
-y_pred, y_true = np.hstack(y_pred), np.hstack(data_val.Y)
+y_pred, y_true = np.hstack(y_pred), np.hstack(data_val['Y'])
 y_pred = y_pred[y_true != 255]
 y_true = y_true[y_true != 255]
 
