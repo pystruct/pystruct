fix bug in test, also do less precision....

pystruct/tests/test_models/test_graph_crf.py
@@ -100,7 +100,7 @@ def test_graph_crf_energy_lp_integral():
     assert_array_almost_equal(np.max(inf_res[0], axis=-1), 1)
     y = np.argmax(inf_res[0], axis=-1)
     # energy and psi check out
-    assert_almost_equal(energy_lp, -np.dot(w, crf.psi((x_1, g_1), y)))
+    assert_almost_equal(energy_lp, -np.dot(w, crf.psi((x_1, g_1), y)), 4)
 
 
 def test_graph_crf_energy_lp_relaxed():

pystruct/tests/test_models/test_latent_node_crf.py
@@ -234,8 +234,9 @@ def test_edge_feature_latent_node_crf_no_latent():
                                        n_edge_features=2, n_hidden_states=5)
         w = np.hstack([np.eye(3).ravel(), -pw_horz.ravel(), -pw_vert.ravel()])
         y_pred = crf.inference(x, w, relaxed=True)
-        assert_array_almost_equal(res[0], y_pred[0].reshape(-1, n_states + 5))
-        assert_array_almost_equal(res[1], y_pred[1])
+        assert_array_almost_equal(res[0], y_pred[0].reshape(-1, n_states + 5),
+                                  4)
+        assert_array_almost_equal(res[1], y_pred[1], 4)
         assert_array_equal(y, np.argmax(y_pred[0], axis=-1))
 
     for inference_method in get_installed(["lp", "ad3", "qpbo"]):