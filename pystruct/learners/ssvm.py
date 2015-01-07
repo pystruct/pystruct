0c4173319c7b679f2d589e43407f3308af2be111
Rob <robertemealey@gmail.com>
2015-01-07 17:27:28 +0000

    remove parallel logic from ssvm.py

--- pystruct/learners/ssvm.py
+++ pystruct/learners/ssvm.py
@@ -1,16 +1,5 @@
 
 import numpy as np
-import warnings
-from multiprocessing.dummy import Pool as ThreadPool
-try:
-    from sklearn.externals.joblib.pool import (MemmapingPool, Pool) 
-except ImportError:
-    from multiprocessing import Pool
-    MemmapingPool = False 
-    warnings.warn("your scikit-learn version does not include "
-                  "MemmapingPool, all parallelization using "
-                  "multiprocessing.Pool")
-from sklearn.externals.joblib import cpu_count
 from sklearn.base import BaseEstimator
 
 from ..utils import inference, inference_map, objective_primal
@@ -35,32 +24,6 @@ class BaseSSVM(BaseEstimator):
         ## spawn pool in init, every instance should have pool attribute
         self._spawn_pool()
         
-
-    def _spawn_pool(self):
-        """spawn pool attribute."""
-        if self.n_jobs == -1:
-            self._n_jobs = cpu_count()
-        else:
-            self._n_jobs = self.n_jobs
-        if any([self.n_jobs == 1, self.use_threads]):
-            self.pool = ThreadPool(processes=self._n_jobs)
-        elif all([self.use_memmapping_pool, MemmapingPool]):
-            self.pool = MemmapingPool(processes=self._n_jobs, 
-                    temp_folder=self.memmapping_temp_folder)
-        else:
-            self.pool = Pool(processes=self._n_jobs)
-
-
-    def __getstate__(self):
-        """strip pool when pickling"""
-        odict = self.__dict__.copy()
-        del odict['pool']
-        return odict
-
-
-    def __setstate__(self, idict):
-        self.__dict__ = idict
-        self._spawn_pool()
 
 
     def predict(self, X):
