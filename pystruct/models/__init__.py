5a12ff6396ac3961d74895d5bf975c5b76d0a5a0
meunier <meunier@coiba.xrce.xeroxlabs.com>
2017-01-11 09:48:15 +0100

    forgot to commit __init__

--- pystruct/models/__init__.py
+++ pystruct/models/__init__.py
@@ -9,9 +9,11 @@ from .latent_node_crf import LatentNodeCRF, EdgeFeatureLatentNodeCRF
 from .unstructured_svm import BinaryClf, MultiClassClf
 from .multilabel_svm import MultiLabelClf
 from .edge_feature_graph_crf import EdgeFeatureGraphCRF
+from .node_type_edge_feature_graph_crf import NodeTypeEdgeFeatureGraphCRF
 
 __all__ = ["StructuredModel", "CRF", "GridCRF", "GraphCRF",
            "DirectionalGridCRF", "BinaryClf", "LatentGridCRF",
            "LatentDirectionalGridCRF", "MultiClassClf", "LatentGraphCRF",
            "MultiLabelClf", "ChainCRF", "LatentNodeCRF", "EdgeFeatureGraphCRF",
-           "EdgeFeatureLatentNodeCRF"]
+           "EdgeFeatureLatentNodeCRF", "NodeTypeEdgeFeatureGraphCRF",
+           "NodeTypeEdgeFeatureGraphCRF"]
