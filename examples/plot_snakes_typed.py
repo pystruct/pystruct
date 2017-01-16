a8260025a49bac56995d354b49c48876d9841e01
meunier <meunier@coiba.xrce.xeroxlabs.com>
2017-01-16 10:47:35 +0100

    The snake example with the NodeTypeEdgeFeatureGraphCRF class

--- examples/plot_snakes_typed.py
+++ examples/plot_snakes_typed.py
@@ -3,8 +3,9 @@
 Conditional Interactions on the Snakes Dataset
 ==============================================
 
-This is a varaint of plot_snakes.py where we use the NodeTypeEdgeFeatureGraphCRF
+This is a variant of plot_snakes.py where we use the NodeTypeEdgeFeatureGraphCRF
 class instead of EdgeFeatureGraphCRF, despite there is only 1 type of nodes.
+So this should give exact same results as plot_snakes.py
 
 
 This example uses the snake dataset introduced in
@@ -61,24 +62,16 @@ if __name__ == '__main__':
     snakes = load_snakes()
     X_train, Y_train = snakes['X_train'], snakes['Y_train']
     
-    #JL
-#     X_train, Y_train = X_train[:40], Y_train[:40]
-#     print len(X_train), len(Y_train)
-#     print X_train[0].shape
-#     print Y_train[0].shape
-    
     X_train = [one_hot_colors(x) for x in X_train]
     Y_train_flat = [y_.ravel() for y_ in Y_train]
-    
+
+
     X_train_directions, X_train_edge_features = prepare_data(X_train)
-    
-    #CHANGE!!
-    #We require AD3 and  NodeTypeEdgeFeatureGraphCRF
-    #inference = 'qpbo'
+
+    inference = 'qpbo'
     # first, train on X with directions only:
-    #crf = NodeTypeEdgeFeatureGraphCRF(inference_method=inference)
-    crf = NodeTypeEdgeFeatureGraphCRF(1, [11], [45], [[2]])
-    ssvm = OneSlackSSVM(crf, inference_cache=50, C=.1, tol=.1, max_iter=100,
+    crf = NodeTypeEdgeFeatureGraphCRF(1, [11], [45], [[2]], inference_method=inference)
+    ssvm = OneSlackSSVM(crf, inference_cache=50, C=.1, tol=.1,  max_iter=100,
                         n_jobs=1)
     ssvm.fit(convertToSingleTypeX(X_train_directions), Y_train_flat)
     
@@ -93,13 +86,10 @@ if __name__ == '__main__':
     print("Test accuracy: %.3f"
           % accuracy_score(np.hstack(Y_test_flat), np.hstack(Y_pred)))
     print(confusion_matrix(np.hstack(Y_test_flat), np.hstack(Y_pred)))
-    
+       
     # now, use more informative edge features:
-    crf = NodeTypeEdgeFeatureGraphCRF(1, [11], [45], [[180]])
-    ssvm = OneSlackSSVM(crf, inference_cache=50, C=.1, tol=.1,
-                        #switch_to='ad3',
-                        #CHANGE: AD3 by default and only 100 iterations to save time and energy...
-                        max_iter=100,
+    crf = NodeTypeEdgeFeatureGraphCRF(1, [11], [45], [[180]], inference_method=inference)
+    ssvm = OneSlackSSVM(crf, inference_cache=50, C=.1, tol=.1,  switch_to='ad3',
                         n_jobs=-1)
     ssvm.fit( convertToSingleTypeX(X_train_edge_features), Y_train_flat)
     Y_pred2 = ssvm.predict( convertToSingleTypeX(X_test_edge_features) )
@@ -108,19 +98,51 @@ if __name__ == '__main__':
           % accuracy_score(np.hstack(Y_test_flat), np.hstack(Y_pred2)))
     print(confusion_matrix(np.hstack(Y_test_flat), np.hstack(Y_pred2)))
     
-    # plot stuff
-    fig, axes = plt.subplots(2, 2)
-    axes[0, 0].imshow(snakes['X_test'][0], interpolation='nearest')
-    axes[0, 0].set_title('Input')
-    y = Y_test[0].astype(np.int)
-    bg = 2 * (y != 0)  # enhance contrast
-    axes[0, 1].matshow(y + bg, cmap=plt.cm.Greys)
-    axes[0, 1].set_title("Ground Truth")
-    axes[1, 0].matshow(Y_pred[0].reshape(y.shape) + bg, cmap=plt.cm.Greys)
-    axes[1, 0].set_title("Prediction w/o edge features")
-    axes[1, 1].matshow(Y_pred2[0].reshape(y.shape) + bg, cmap=plt.cm.Greys)
-    axes[1, 1].set_title("Prediction with edge features")
-    for a in axes.ravel():
-        a.set_xticks(())
-        a.set_yticks(())
-    plt.show()
+    if False:
+        # plot stuff
+        fig, axes = plt.subplots(2, 2)
+        axes[0, 0].imshow(snakes['X_test'][0], interpolation='nearest')
+        axes[0, 0].set_title('Input')
+        y = Y_test[0].astype(np.int)
+        bg = 2 * (y != 0)  # enhance contrast
+        axes[0, 1].matshow(y + bg, cmap=plt.cm.Greys)
+        axes[0, 1].set_title("Ground Truth")
+        axes[1, 0].matshow(Y_pred[0].reshape(y.shape) + bg, cmap=plt.cm.Greys)
+        axes[1, 0].set_title("Prediction w/o edge features")
+        axes[1, 1].matshow(Y_pred2[0].reshape(y.shape) + bg, cmap=plt.cm.Greys)
+        axes[1, 1].set_title("Prediction with edge features")
+        for a in axes.ravel():
+            a.set_xticks(())
+            a.set_yticks(())
+        plt.show()
+
+"""
+Please be patient. Learning will take 5-20 minutes.
+Results using only directional features for edges
+Test accuracy: 0.847
+[[2750    0    0    0    0    0    0    0    0    0    0]
+ [   0   99    0    0    1    0    0    0    0    0    0]
+ [   0    2   68    3    9    4    6    4    3    1    0]
+ [   0    4   11   45    8   14    5    6    0    6    1]
+ [   0    1   22   18   31    2   14    4    3    5    0]
+ [   0    3    7   38   12   22    5    4    2    7    0]
+ [   0    2   19   16   26    8   16    2    9    2    0]
+ [   0    6   14   26   10   15    5   12    2   10    0]
+ [   0    0   12   15   16    4   16    2   18    4   13]
+ [   0    2    5   18    6    8    5    3    2   50    1]
+ [   0    1   11    4   13    1    2    0    2    2   64]]
+Results using also input features for edges
+Test accuracy: 0.998
+[[2749    0    0    0    0    0    0    0    1    0    0]
+ [   0  100    0    0    0    0    0    0    0    0    0]
+ [   0    0  100    0    0    0    0    0    0    0    0]
+ [   0    0    0   99    0    0    0    0    0    1    0]
+ [   0    0    0    0   99    0    1    0    0    0    0]
+ [   0    0    0    1    0   98    0    1    0    0    0]
+ [   0    0    0    0    1    0   99    0    0    0    0]
+ [   0    0    0    0    0    1    0   99    0    0    0]
+ [   0    0    0    0    0    0    0    0  100    0    0]
+ [   0    0    0    0    0    0    0    1    0   99    0]
+ [   0    0    0    0    0    0    0    0    0    0  100]]
+
+"""
