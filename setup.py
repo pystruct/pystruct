e74165963e0014729b36a0df6668f0b589d9d82c
Jean-Luc Meunier <jean-luc.meunier@xrce.xerox.com>
2017-02-07 10:13:23 +0100

    v0.3.0 requiring ad3 2.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ include_dirs = [np.get_include()]
 
 setup(name="pystruct",
       version="0.3.0",
-      install_requires=["ad3"],
+      install_requires=["ad3>=2.1.0"],
       packages=['pystruct', 'pystruct.learners', 'pystruct.inference',
                 'pystruct.models', 'pystruct.utils', 'pystruct.datasets',
                 'pystruct.tests', 'pystruct.tests.test_learners',
