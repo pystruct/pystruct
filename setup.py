9e72ed2f93055c69a44de0bed1f23fd169ba3fa5
Jean-Luc Meunier <jean-luc.meunier@xrce.xerox.com>
2017-02-20 13:57:46 +0100

    0.3.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if os.path.exists('MANIFEST'):
 include_dirs = [np.get_include()]
 
 setup(name="pystruct",
-      version="0.3.3",
+      version="0.3.4",
       install_requires=["ad3>=2.1.2"],
       packages=['pystruct', 'pystruct.learners', 'pystruct.inference',
                 'pystruct.models', 'pystruct.utils', 'pystruct.datasets',
