57ec6b75ee995e6f19e0db60ad1ee3fdfd1a2ee1
Jean-Luc Meunier <jean-luc.meunier@xrce.xerox.com>
2017-02-20 13:29:41 +0100

    MIT license

--- pystruct/models/node_type_edge_feature_graph_crf.py
+++ pystruct/models/node_type_edge_feature_graph_crf.py
@@ -5,19 +5,23 @@
     
     Copyright Xerox(C) 2017 JL. Meunier
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
     
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
     
     Developed  for the EU project READ. The READ project has received funding 
     from the European Union's Horizon 2020 research and innovation programme 
@@ -255,7 +259,7 @@ class NodeTypeEdgeFeatureGraphCRF(TypedCRF):
 
         """
         self._check_size_x(x)   #call initialize once!
-        l_node_features = self._get_node_features(x, True)
+        l_node_features = self._get_node_features(x)
         l_edges, l_edge_features = self._get_edges(x), self._get_edge_features(x)
         l_n_nodes = [len(nf) for nf in self._get_node_features(x)]
         l_n_edges = [len(ef) for ef in self._get_edges        (x)]
