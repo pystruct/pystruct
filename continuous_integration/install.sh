fa258e0109f6cab232e4fc20bee8f5cfde5989bb
MarcoFalke <falke.marco@gmail.com>
2017-03-16 12:28:54 +0100

    Update install.sh
    Fall back to cvxopt version 1.1.8 for a straightforward fix of the travis issue

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -52,7 +52,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
 elif [[ "$DISTRIB" == "ubuntu" ]]; then
     # Use standard ubuntu packages in their default version
     # except for cython :-/
-    $PIP install --user cvxopt
+    $PIP install --user -Iv cvxopt==1.1.8
 fi
 
 if [[ "$COVERAGE" == "true" ]]; then
