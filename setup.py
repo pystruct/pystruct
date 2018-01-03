b0f0bc694f1759bba1758c1a43824817d06b1762
Jean-Luc Meunier <jean-luc.meunier@naverlabs.com>
2018-01-03 17:16:00 +0100

    install_requires=["ad3", "numpy"]

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ include_dirs = [np.get_include()]
 
 setup(name="pystruct",
       version="0.3.6",
-      install_requires=["ad3>=2.1.2"],
+      install_requires=["ad3", "numpy"],
       packages=['pystruct', 'pystruct.learners', 'pystruct.inference',
                 'pystruct.models', 'pystruct.utils', 'pystruct.datasets',
                 'pystruct.tests', 'pystruct.tests.test_learners',
