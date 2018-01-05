2ade10a7e31b58a4f942aa0c52882aa3cfe77823
Jean-Luc Meunier <jean-luc.meunier@naverlabs.com>
2018-01-05 16:56:17 +0100

    python3.5

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -51,6 +51,28 @@ if [[ "$DISTRIB" == "conda" ]]; then
 
     source activate testenv
 
+elif [[ "$DISTRIB" == "conda3" ]]; then
+    # Deactivate the travis-provided virtual environment and setup a
+    # conda-based environment instead
+    deactivate
+
+    # Use the miniconda installer for faster download / install of conda
+    # itself
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+        -O miniconda.sh
+    chmod +x miniconda.sh && ./miniconda.sh -b -p $HOME/miniconda3
+    export PATH=$HOME/miniconda3/bin:$PATH
+    conda update --yes conda
+
+    # Configure the conda environment and put it in the path using the
+    # provided versions
+    
+    conda create -n testenv --yes python=$PYTHON_VERSION pip nose cython\
+        scikit-learn cvxopt pytest future \
+        numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION
+
+    source activate testenv
+
 elif [[ "$DISTRIB" == "ubuntu" ]]; then
     # Use standard ubuntu packages in their default version
     # except for cython :-/
