cc1600612b8b1357c0a7025fa82bfbf427046899
lison <lison.lemoine@gmail.com>
2021-09-22 21:19:55 -0400

    removed pyqpbo as requirement due to problems in other OS

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ include_dirs = [np.get_include()]
 
 setup(name="pystruct",
       version="0.3.2",
-      install_requires=["ad3", "numpy", "cvxopt", "future", "Cython", "scikit-learn", "pyqpbo"],
+      install_requires=["ad3", "numpy", "cvxopt", "future", "Cython", "scikit-learn"],
       packages=['pystruct', 'pystruct.learners', 'pystruct.inference',
                 'pystruct.models', 'pystruct.utils', 'pystruct.datasets',
                 'pystruct.tests', 'pystruct.tests.test_learners',
